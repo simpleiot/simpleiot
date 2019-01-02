d02f2ed5ea507ed353eba64481accede5d4a91d9
collin <collin@bec-systems.com>
2019-01-02 17:25:18 -0500

    made keypad/run.go get characters from keyboard and send out Key type also filled in string messages in isdata/data.go

--- farmation/keypad/run.go
+++ farmation/keypad/run.go
@@ -1,19 +1,58 @@
 package keypad
 
 import (
+	"bytes"
+	"fmt"
+
+	"github.com/pkg/term"
 	"github.com/simpleiot/simpleiot/data"
 	"github.com/simpleiot/simpleiot/farmation/isdata"
 )
 
+// Get character as a byte slice
+func getch() []byte {
+	t, _ := term.Open("/dev/tty")
+	term.RawMode(t)
+	bytes := make([]byte, 3)
+	numRead, err := t.Read(bytes)
+	t.Restore()
+	t.Close()
+	if err != nil {
+		return nil
+	}
+	return bytes[0:numRead]
+}
+
 // Run goroutine for keypad code
 func Run(in, out chan interface{}) {
 	go func() {
-		out <- isdata.KeyLeft
-		//for {
-		// insert code to read keypad
-		// once you have key
-		// out <- isdata.KeyLeft
-		//}
+		for {
+			c := getch()
+			switch {
+			case bytes.Equal(c, []byte{3}):
+				return
+			case bytes.Equal(c, []byte{27, 91, 65}): // up arrow
+				out <- isdata.KeyUp
+			case bytes.Equal(c, []byte{27, 91, 66}): // down
+				out <- isdata.KeyDown
+			case bytes.Equal(c, []byte{27, 91, 67}): // right
+				out <- isdata.KeyRight
+			case bytes.Equal(c, []byte{27, 91, 68}): // left
+				out <- isdata.KeyLeft
+			case bytes.Equal(c, []byte{13}): // enter
+				out <- isdata.KeyEnter
+			case bytes.Equal(c, []byte{49}): // 1
+				out <- isdata.KeySK1
+			case bytes.Equal(c, []byte{50}): // 2
+				out <- isdata.KeySK2
+			case bytes.Equal(c, []byte{51}): // 3
+				out <- isdata.KeySK3
+			case bytes.Equal(c, []byte{52}): // 4
+				out <- isdata.KeySK4
+			default:
+				fmt.Println("unknown key", c)
+			}
+		}
 	}()
 	for {
 		select {
