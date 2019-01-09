c1b5161c5deaf722acee66ff33f579bee212b8f6
collin <collin@bec-systems.com>
2019-01-09 16:02:12 -0500

    added pushbutton gpio name

--- farmation/keypad/run.go
+++ farmation/keypad/run.go
@@ -55,6 +55,7 @@ func Run(in, out chan interface{}) {
 
 	go keypad(out, "PB0", isdata.KeyEnter)
 	go keypad(out, "PD25", isdata.KeySK1)
+	go keypad(out, "PB4", isdata.KeyUp)
 	go func() {
 		for {
 			c := getch()
