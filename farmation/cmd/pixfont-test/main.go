6449abce88fdf94ce5af7afa351acaac3db85124
Cliff Brake <cbrake@bec-systems.com>
2019-01-25 12:34:00 -0500

    drop images and use new GetHeight api

--- farmation/cmd/pixfont-test/main.go
+++ farmation/cmd/pixfont-test/main.go
@@ -13,12 +13,13 @@ import (
 func main() {
 	txt := "ai"
 	width := tightpixel15.Font.MeasureString(txt)
+	height := tightpixel15.Font.GetHeight()
 	fmt.Println("width: ", width)
-	img := image.NewRGBA(image.Rect(0, 0, width, 10))
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
 	widthActual := tightpixel15.Font.DrawString(img, 0, 0, txt, color.Black)
 	imgS := img.SubImage(image.Rectangle{
 		image.Point{0, 0},
-		image.Point{widthActual, 10},
+		image.Point{widthActual, height},
 	})
 	fmt.Println("widthActual: ", widthActual)
 
--- farmation/isui/txt.go
+++ farmation/isui/txt.go
@@ -11,9 +11,16 @@ import (
 // RenderTxt renders text into a Blt
 func RenderTxt(x, y int, txt string) isdata.LcdBlt {
 	width := tightpixel15.Font.MeasureString(txt)
-	img := image.NewRGBA(image.Rect(0, 0, width, 10))
-	tightpixel15.Font.DrawString(img, 0, 0, txt, color.Black)
+	height := tightpixel15.Font.GetHeight()
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	widthActual := tightpixel15.Font.DrawString(img, 0, 0, txt, color.Black)
+	imgCropped := img.SubImage(
+		image.Rectangle{
+			image.Point{0, 0},
+			image.Point{widthActual, height},
+		},
+	)
 	//f, _ := os.OpenFile("RenderTxt.png", os.O_CREATE|os.O_RDWR, 0644)
 	//png.Encode(f, img)
-	return ImageToBltA(x, y, img, true)
+	return ImageToBltA(x, y, imgCropped, true)
 }
