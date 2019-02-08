ee00e75d1b043e5fe550080ee3800ca8176d849b
Cliff Brake <cbrake@bec-systems.com>
2019-02-08 12:19:56 -0500

    example Sample type definition and document

--- data/sample.go
+++ data/sample.go
@@ -4,9 +4,24 @@ import "time"
 
 // Sample represents a value in time
 type Sample struct {
-	ID    string    `json:"id"`
-	Value float64   `json:"value"`
-	Time  time.Time `json:"time"`
+	// Type of sample (voltage, current, key, etc)
+	Type string `json:"type,omitempty"`
+
+	// ID of the device that provided the sample
+	ID string `json:"id,omitempty"`
+
+	// Analog or digital value of the sample. 0 and 1 are used
+	// to represent digital values
+	Value float64 `json:"value,omitempty"`
+
+	// Time the sample was taken
+	Time time.Time `json:"time,omitempty"`
+
+	// Duration over which the sample was taken
+	Duration time.Duration `json:"duration,omitempty"`
+
+	// Tags are additional attributes used to describe the sample
+	Tags map[string]string `json:"tags,omitempty"`
 }
 
 // NewSample creates a new sample at current time
