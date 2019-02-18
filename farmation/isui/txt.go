ebefd9a4323af2f077e90a438394f1ede1d7dd45
Cliff Brake <cbrake@bec-systems.com>
2019-02-18 15:19:27 -0500

    add justification options for txt, and heading function

--- farmation/isui/txt.go
+++ farmation/isui/txt.go
@@ -15,7 +15,28 @@ func DrawTxt(img draw.Image, txt string, x, y int, font *pixfont.PixFont) {
 	font.DrawString(img, x, y, txt, color.Black)
 }
 
-// RenderTxt renders text into an image
+// DrawTxtCentered draws text into an image centered around x,y
+func DrawTxtCentered(img draw.Image, txt string, x, y int, font *pixfont.PixFont) {
+	length := font.MeasureString(txt)
+	x -= length / 2
+	font.DrawString(img, x, y, txt, color.Black)
+}
+
+// DrawTxtRight draws right justified text with x,y at end of string
+func DrawTxtRight(img draw.Image, txt string, x, y int, font *pixfont.PixFont) {
+	length := font.MeasureString(txt)
+	x -= length
+	font.DrawString(img, x, y, txt, color.Black)
+}
+
+// Heading draws the screen heading with a box around it
+func Heading(img draw.Image, txt string) {
+	DrawTxtCentered(img, txt, 64, 2, tightpixel15.Font)
+	width := tightpixel15.Font.MeasureString(txt)
+	Rect(img, 64-width/2-2, 0, width+2, 11)
+}
+
+// RenderTxt returns an image of rendered text
 func RenderTxt(txt string) image.Image {
 	width := tightpixel15.Font.MeasureString(txt)
 	height := tightpixel15.Font.GetHeight()
