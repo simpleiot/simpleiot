a49ae1436527c43de6569cf4c9ebda26cfb07d0e
Cliff Brake <cbrake@bec-systems.com>
2019-02-22 10:17:22 -0500

    add examples

--- farmation/isui/screen-field-menu.go
+++ farmation/isui/screen-field-menu.go
@@ -3,16 +3,19 @@ package isui
 import (
 	"image/draw"
 
+	"github.com/simpleiot/simpleiot/farmation/fonts/tightpixel15"
 	"github.com/simpleiot/simpleiot/farmation/isdata"
 )
 
 // FieldMenuScreen is used to display status info
 type FieldMenuScreen struct {
-	softKeys *SoftKeys
-	state    *isdata.State
-	config   *isdata.Config
-	arrowPos int
-	menu     Menu
+	softKeys     *SoftKeys
+	softKeysEdit *SoftKeys
+	state        *isdata.State
+	config       *isdata.Config
+	arrowPos     int
+	menu         Menu
+	edit         bool
 }
 
 // NewFieldMenuScreen initializes and returns a HomeScreen
@@ -24,28 +27,45 @@ func NewFieldMenuScreen(state *isdata.State, config *isdata.Config) *FieldMenuSc
 	menu.AddItemScreen("Field Four", ScreenIDNoChange)
 
 	return &FieldMenuScreen{
-		softKeys: NewSoftKeys("back", "edit", "import"),
-		state:    state,
-		config:   config,
-		menu:     menu,
+		softKeys:     NewSoftKeys("back", "edit", "import"),
+		softKeysEdit: NewSoftKeys("done", "bkspc", "ABC", "cancel"),
+		state:        state,
+		config:       config,
+		menu:         menu,
 	}
 }
 
 // Render updates the home screen, and provides an image
 func (s *FieldMenuScreen) Render(img draw.Image) {
 	Clear(img)
-	Heading(img, "Field Menu")
-	s.menu.Render(img)
-	s.softKeys.Render(img, 0, 54)
+	if s.edit {
+		Line(img, 10, 10, 40, 40)
+		s.softKeysEdit.Render(img, 0, 54)
+
+	} else {
+		Heading(img, "Field Menu")
+		s.menu.Render(img)
+		s.softKeys.Render(img, 0, 54)
+	}
+	DrawTxt(img, "hi collin", 50, 50, tightpixel15.Font)
 }
 
 // Key processes keypad input to this screen
 func (s *FieldMenuScreen) Key(key isdata.Key) (ScreenID, interface{}) {
-	switch key {
-	case isdata.KeySK1:
-		return ScreenIDMainMenu, nil
-	case isdata.KeyUp, isdata.KeyDown, isdata.KeyRight, isdata.KeyLeft, isdata.KeyEnter:
-		return s.menu.Key(key)
+	if s.edit {
+		switch key {
+		case isdata.KeySK4:
+			s.edit = false
+		}
+	} else {
+		switch key {
+		case isdata.KeySK1:
+			return ScreenIDMainMenu, nil
+		case isdata.KeySK2:
+			s.edit = true
+		case isdata.KeyUp, isdata.KeyDown, isdata.KeyRight, isdata.KeyLeft, isdata.KeyEnter:
+			return s.menu.Key(key)
+		}
 	}
 
 	return ScreenIDNoChange, nil
