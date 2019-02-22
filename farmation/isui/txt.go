8523d2debeba5691ec6b2ce4b037e7c471e40afb
Cliff Brake <cbrake@bec-systems.com>
2019-02-22 10:07:10 -0500

    remove unused txt functions

--- farmation/isui/txt.go
+++ farmation/isui/txt.go
@@ -1,13 +1,11 @@
 package isui
 
 import (
-	"image"
 	"image/color"
 	"image/draw"
 
 	"github.com/pbnjay/pixfont"
 	"github.com/simpleiot/simpleiot/farmation/fonts/tightpixel15"
-	"github.com/simpleiot/simpleiot/farmation/isdata"
 )
 
 // DrawTxt draws text into an image
@@ -34,23 +32,4 @@ func Heading(img draw.Image, txt string) {
 	DrawTxtCentered(img, txt, 64, 2, tightpixel15.Font)
 	width := tightpixel15.Font.MeasureString(txt)
 	Rect(img, 64-width/2-2, 0, width+2, 11)
-}
-
-// RenderTxt returns an image of rendered text
-func RenderTxt(txt string) image.Image {
-	width := tightpixel15.Font.MeasureString(txt)
-	height := tightpixel15.Font.GetHeight()
-	img := image.NewRGBA(image.Rect(0, 0, width, height))
-	tightpixel15.Font.DrawString(img, 0, 0, txt, color.Black)
-	//imgCropped := img.SubImage(image.Rect(0, 0, widthActual, height))
-
-	//f, _ := os.OpenFile("RenderTxt.png", os.O_CREATE|os.O_RDWR, 0644)
-	//png.Encode(f, img)
-	return img
-}
-
-// RenderTxtBlt renders text into a Blt
-func RenderTxtBlt(x, y int, txt string) isdata.LcdBlt {
-	img := RenderTxt(txt)
-	return ImageToBltA(x, y, img, true)
 }
