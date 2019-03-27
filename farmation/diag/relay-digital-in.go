6c4a00282170b093b9bbbdf8fab657bc2eecf367
Cliff Brake <cbrake@bec-systems.com>
2019-03-27 15:35:52 -0400

    finish relay/dig in diags

--- farmation/diag/relay-digital-in.go
+++ farmation/diag/relay-digital-in.go
@@ -3,6 +3,7 @@ package diag
 import (
 	"errors"
 	"log"
+	"time"
 
 	"github.com/simpleiot/simpleiot/farmation/isio"
 )
@@ -45,9 +46,60 @@ func (d relayDigitalIn) Run() (ret error) {
 	// activate one relay driver at a time and verify digital
 	// inputs
 	isio.GpioOut(isio.GpioRelayInjectorEn, true)
+	time.Sleep(100 * time.Millisecond)
 
 	if isio.GpioRead(isio.GpioDigitalInjector) != true {
 		log.Println("injector gpio is not active")
+		ret = errDigitalIn
+	}
+
+	isio.GpioOut(isio.GpioRelayInjectorEn, false)
+	time.Sleep(100 * time.Millisecond)
+
+	if isio.GpioRead(isio.GpioDigitalInjector) != false {
+		log.Println("injector gpio is active")
+		ret = errDigitalIn
+	}
+
+	isio.GpioOut(isio.GpioRelayAuxEn, true)
+	time.Sleep(100 * time.Millisecond)
+
+	if isio.GpioRead(isio.GpioDigitalIrrigator) != true {
+		log.Println("irrigator gpio is not active")
+		ret = errDigitalIn
+	}
+
+	isio.GpioOut(isio.GpioRelayAuxEn, false)
+	time.Sleep(100 * time.Millisecond)
+
+	if isio.GpioRead(isio.GpioDigitalIrrigator) != false {
+		log.Println("irrigator gpio is active")
+		ret = errDigitalIn
+	}
+
+	isio.GpioOut(isio.GpioRelayShutdownEn, true)
+	time.Sleep(100 * time.Millisecond)
+
+	if isio.GpioRead(isio.GpioDigitalWaterOn) != true {
+		log.Println("water gpio is not active")
+		ret = errDigitalIn
+	}
+
+	if isio.GpioRead(isio.GpioDigitalIn) != true {
+		log.Println("digital in gpio is not active")
+		ret = errDigitalIn
+	}
+
+	isio.GpioOut(isio.GpioRelayShutdownEn, false)
+	time.Sleep(100 * time.Millisecond)
+
+	if isio.GpioRead(isio.GpioDigitalWaterOn) != false {
+		log.Println("water gpio is active")
+		ret = errDigitalIn
+	}
+
+	if isio.GpioRead(isio.GpioDigitalIn) != false {
+		log.Println("digital in gpio is active")
 		ret = errDigitalIn
 	}
 
