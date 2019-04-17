0fc4d70d062cf593a6b05164f59e91aa3ddfdb60
Cliff Brake <cbrake@bec-systems.com>
2019-04-16 22:26:23 -0400

    add usb and reset test

--- farmation/diag/modem.go
+++ farmation/diag/modem.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/cbrake/go-serial/serial"
 	"github.com/simpleiot/simpleiot/farmation/isio"
+	"github.com/simpleiot/simpleiot/file"
 )
 
 type modem struct{}
@@ -14,10 +15,16 @@ func (d modem) String() string {
 	return "modem"
 }
 
+const modemUsbDev = "/dev/cdc-wdm0"
+
 func (d modem) Run() error {
 	isio.GpioOut(isio.GpioModemSleep, false)
 	isio.GpioOut(isio.GpioModemReset, true)
-	time.Sleep(30 * time.Millisecond)
+	time.Sleep(2 * time.Second)
+
+	if file.Exists(modemUsbDev) {
+		return errors.New("modem reset is not working")
+	}
 
 	options := serial.OpenOptions{
 		PortName:              isio.SerialModem,
@@ -36,18 +43,15 @@ func (d modem) Run() error {
 
 	defer p.Close()
 
-	err = DigiCheckAt(p)
-
-	if err == nil {
-		return errors.New("Modem is reponding when reset is asserted")
-	}
-
 	isio.GpioOut(isio.GpioModemReset, false)
 
 	// modem takes about 5 seconds to show up on USB bus after reset
-	time.Sleep(5 * time.Second)
+	time.Sleep(6 * time.Second)
 
-	DigiCheckAt(p)
+	if !file.Exists(modemUsbDev) {
+		return errors.New("Modem is not detected on USB bus")
+	}
+
 	return DigiCheckAt(p)
 }
 
