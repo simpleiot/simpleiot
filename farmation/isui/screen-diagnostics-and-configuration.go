80ac00daa700844ce894d7dd81447c035e20a49c
collin <collin@bec-systems.com>
2019-04-26 16:45:49 -0400

    diagnostics and ethernet screens -copies of totals

---
+++ farmation/isui/screen-diagnostics-and-configuration.go
@@ -0,0 +1,51 @@
+package isui
+
+import (
+	"image/draw"
+
+	"github.com/simpleiot/simpleiot/farmation/isdata"
+)
+
+// DiagnosticsScreen diagnostics screen
+type DiagnosticsScreen struct {
+	softKeys *SoftKeys
+	state    *isdata.State
+	config   *isdata.Config
+	arrowPos int
+	menu     Menu
+}
+
+// NewDiagnosticsScreen gives new Diagnostics and Configuration screen to screen.go
+func NewDiagnosticsScreen(state *isdata.State, config *isdata.Config) *DiagnosticsScreen {
+	isdata.InitState(state) // !!! comment from sample screen !!! make sure that ProductStates and FieldStates arrays are large enough
+	menu := Menu{}
+
+	return &DiagnosticsScreen{
+		// update this from sample screen
+		softKeys: NewSoftKeys("back"),
+		state:    state,
+		config:   config,
+		menu:     menu,
+	}
+}
+
+// Render updates the home screen, and provides an image
+func (s *DiagnosticsScreen) Render(img draw.Image) {
+	Clear(img)
+	Heading(img, "Diagnostics and Configuration")
+	s.menu.Render(img)
+	s.softKeys.Render(img, 0, 54)
+}
+
+// Key processes keypad input to this screen
+func (s *DiagnosticsScreen) Key(key isdata.Key) (ScreenID, interface{}) {
+	switch key {
+	case isdata.KeySK1:
+		s.menu.ResetArrowPos() // return arrow to top of screen
+		return ScreenIDMainMenu, nil
+	case isdata.KeyUp, isdata.KeyDown, isdata.KeyRight, isdata.KeyLeft, isdata.KeyEnter:
+		return s.menu.Key(key)
+	}
+
+	return ScreenIDNoChange, nil
+}
---
+++ farmation/isui/screen-ethernet.go
@@ -0,0 +1,51 @@
+package isui
+
+import (
+	"image/draw"
+
+	"github.com/simpleiot/simpleiot/farmation/isdata"
+)
+
+// EthernetScreen ethernet screen
+type EthernetScreen struct {
+	softKeys *SoftKeys
+	state    *isdata.State
+	config   *isdata.Config
+	arrowPos int
+	menu     Menu
+}
+
+// NewEthernetScreen gives new Ethernet screen to screen.go
+func NewEthernetScreen(state *isdata.State, config *isdata.Config) *EthernetScreen {
+	isdata.InitState(state) // make sure that ProductStates and FieldStates arrays are large enough
+	menu := Menu{}
+
+	return &EthernetScreen{
+		// update this from sample screen
+		softKeys: NewSoftKeys("back"),
+		state:    state,
+		config:   config,
+		menu:     menu,
+	}
+}
+
+// Render updates the home screen, and provides an image
+func (s *EthernetScreen) Render(img draw.Image) {
+	Clear(img)
+	Heading(img, "Ethernet")
+	s.menu.Render(img)
+	s.softKeys.Render(img, 0, 54)
+}
+
+// Key processes keypad input to this screen
+func (s *EthernetScreen) Key(key isdata.Key) (ScreenID, interface{}) {
+	switch key {
+	case isdata.KeySK1:
+		s.menu.ResetArrowPos() // return arrow to top of screen
+		return ScreenIDMainMenu, nil
+	case isdata.KeyUp, isdata.KeyDown, isdata.KeyRight, isdata.KeyLeft, isdata.KeyEnter:
+		return s.menu.Key(key)
+	}
+
+	return ScreenIDNoChange, nil
+}
