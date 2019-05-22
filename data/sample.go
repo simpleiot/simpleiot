a4bdcca83f7e5ef7d80b9ef4710c271bf0ee4f23
Cliff Brake <cbrake@bec-systems.com>
2019-05-22 15:43:06 -0400

    add sample attributes and expand constructor

--- data/sample.go
+++ data/sample.go
@@ -22,12 +22,16 @@ type Sample struct {
 
 	// Tags are additional attributes used to describe the sample
 	Tags map[string]string `json:"tags,omitempty"`
+
+	// Attributes are additional values
+	Attributes map[string]float64 `json:"attributes,omitempty"`
 }
 
 // NewSample creates a new sample at current time
-func NewSample(ID string, value float64) Sample {
+func NewSample(ID, sampleType string, value float64) Sample {
 	return Sample{
 		ID:    ID,
+		Type:  sampleType,
 		Value: value,
 		Time:  time.Now(),
 	}
