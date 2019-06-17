d6c874f7369d29290d985dc2431fe83d962c3eed
Cliff Brake <cbrake@bec-systems.com>
2019-06-17 15:51:22 -0400

    build lindsay_sim

--- is_envsetup.sh
+++ is_envsetup.sh
@@ -70,6 +70,7 @@ is_build_arm() {
   is_build_dependencies || return 1
   GOARCH=arm go build -o is_arm farmation/cmd/injector-sentry/main.go || return 1
   GOARCH=arm go build -o lcd_test farmation/cmd/lcd-test/main.go || return 1
+  GOARCH=arm go build -o lindsay_sim farmation/cmd/lindsay-sim/main.go || return 1
   return 0
 }
 
