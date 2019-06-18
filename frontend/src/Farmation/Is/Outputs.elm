fff7135c39130f57f590af007a93b56836a2e967
Cliff Brake <cbrake@bec-systems.com>
2019-06-18 17:17:25 -0400

    add display of LED state

---
+++ frontend/src/Farmation/Is/Outputs.elm
@@ -0,0 +1,38 @@
+module Farmation.Is.Outputs exposing (Msg(..), statusLed)
+
+import Html
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+
+
+type Msg
+    = NoOp
+
+
+statusLed : Bool -> Bool -> Html.Html Msg
+statusLed red green =
+    let
+        color =
+            if red then
+                "red"
+
+            else if green then
+                "green"
+
+            else
+                "grey"
+    in
+    svg
+        [ width "55"
+        , height "55"
+        ]
+        [ circle
+            [ cx "27"
+            , cy "27"
+            , r "25"
+            , fill color
+            , stroke "black"
+            , strokeWidth "3"
+            ]
+            []
+        ]
