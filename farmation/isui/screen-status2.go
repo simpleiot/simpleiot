4decc5a714b2391189ca60d1fbbd6b86f67b2678
Cliff Brake <cbrake@bec-systems.com>
2019-06-19 16:36:24 -0400

    control relays with state

--- farmation/isui/screen-status2.go
+++ farmation/isui/screen-status2.go
@@ -38,7 +38,7 @@ func (s *StatusScreen2) Render(img draw.Image) {
 	// icons
 	s.icons.SetPage("page indicator", 2)
 	s.icons.SetOnOff("arm", s.config.Arm)
-	s.icons.SetOnOff("pump", s.config.RelayInjector)
+	s.icons.SetOnOff("pump", s.state.GpioRelayInjectorEn)
 	s.icons.SetOnOff("water", s.state.GpioDigitalWaterOn)
 	s.icons.Render(img)
 }
