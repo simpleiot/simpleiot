81fde6ab2e84b7365f2ad84c49529aadfa711a0f
Cliff Brake <cbrake@bec-systems.com>
2019-06-19 11:11:26 -0400

    send state to new websocket connections

--- farmation/isapi/server.go
+++ farmation/isapi/server.go
@@ -82,6 +82,7 @@ func Server(in, out chan interface{}) {
 	newConn := make(chan bool)
 
 	frameBuffer := isdata.NewLcdBlt(0, 0, 128, 64, true)
+	state := isdata.State{}
 
 	port := os.Getenv("IS_PORT")
 	if port == "" {
@@ -118,20 +119,27 @@ func Server(in, out chan interface{}) {
 					frameBuffer.Update(m)
 				case isdata.LcdBltSolid:
 					frameBuffer.UpdateSolid(m)
+				case isdata.State:
+					state = m
 				}
 			default:
 				log.Printf("web: unhandled message of type %T: %+v\n", m, m)
 			}
 		case <-newConn:
-			log.Println("New web client connection, sending current FB")
-			d, err := json.Marshal(frameBuffer)
+			log.Println("New web client connection, sending current FB and state")
+			fb, err := json.Marshal(frameBuffer)
 			if err != nil {
-				log.Println("Error encoding Framebuffer: ", err)
-			} else {
-				// give the client time to get set up
-				time.Sleep(10 * time.Millisecond)
-				wsTxChan <- d
+				log.Println("Error encoding Framebuffer for new connection: ", err)
+				continue
 			}
+			st, err := json.Marshal(state)
+			if err != nil {
+				log.Println("Error encoding state for new connection: ", err)
+				continue
+			}
+			time.Sleep(10 * time.Millisecond)
+			wsTxChan <- fb
+			wsTxChan <- st
 		case m := <-wsRxChan:
 			s := data.Sample{}
 			if err := json.Unmarshal(m, &s); err != nil {
