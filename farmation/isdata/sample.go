f1b78d436a49d0b14c595e777b6b4a3556cbd25b
Cliff Brake <cbrake@bec-systems.com>
2019-07-31 15:21:05 -0400

    add note on cleanup

--- farmation/isdata/sample.go
+++ farmation/isdata/sample.go
@@ -2,7 +2,8 @@ package isdata
 
 // Define sample types for the system
 const (
-	SampleTypePressure       string = "pressure"
+	SampleTypePressure string = "pressure"
+	// FIXME, Max/Min/Avg can now be stored directly in the sample
 	SampleTypePressureMax    string = "pressureMax"
 	SampleTypePressureMin    string = "pressureMin"
 	SampleTypePressureAvg    string = "pressureAvg"
