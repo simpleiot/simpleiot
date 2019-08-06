a51b475d9f7778b13f1eaf2f87ca37b023e6a53f
Cliff Brake <cbrake@bec-systems.com>
2019-08-06 15:42:34 -0400

    remove redundant types

--- farmation/isdata/log-sample.go
+++ farmation/isdata/log-sample.go
@@ -3,10 +3,10 @@ package isdata
 // Define sample types for the system
 const (
 	SampleTypePulses            string = "pulses"
-	SampleTypeFlowInstantaneous string = "flowInstantaneous"
-	SampleTypeFlowWindowAvg     string = "flowWindowAvg"
-	SampleTypeAmount            string = "amount"
-	SampleTypePressure          string = "pressure"
-	SampleTypePressureVRef      string = "pressureVRef"
-	SampleTypePressureVSense    string = "pressureVSense"
+	SampleTypeFlowInstantaneous        = "flowInstantaneous"
+	SampleTypeFlowWindowAvg            = "flowWindowAvg"
+	SampleTypeAmount                   = "amount"
+	SampleTypePressure                 = "pressure"
+	SampleTypePressureVRef             = "pressureVRef"
+	SampleTypePressureVSense           = "pressureVSense"
 )
