8b6f686c0f270bb0b0964611ccbfa814dd861e6b
collin <collin@bec-systems.com>
2019-08-07 11:17:39 -0400

    deleted samples sorting methods

--- farmation/isdata/sample.go
+++ farmation/isdata/sample.go
@@ -1,7 +1,5 @@
 package isdata
 
-import "github.com/simpleiot/simpleiot/data"
-
 // Define sample types for the system
 const (
 	SampleTypePulses            string = "pulses"
@@ -19,24 +17,3 @@ const (
 	SampleSubTypeFaultPres            = "lowPressure"
 	SampleSubTypeFaultShutdown        = "shutdownFailed"
 )
-
-// Samples is used to allow samples to be sorted by timestamp
-type Samples []data.Sample
-
-// Below methods allow samples to be automatically sorted by timestamp
-
-// Len return length of slice
-func (s Samples) Len() int {
-	return len(s)
-}
-
-// Swap positions i and j
-func (s Samples) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
-}
-
-// Less returns whether timestamp of sample at pos i is after timestamp
-// of fault at pos j
-func (s Samples) Less(i, j int) bool {
-	return s[i].Time.Before(s[j].Time)
-}
