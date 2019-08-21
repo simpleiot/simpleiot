417c8538ad243012213193d38d081bf369c38685
Cliff Brake <cbrake@bec-systems.com>
2019-08-21 17:12:08 -0400

    update docs

--- respreader/doc.go
+++ respreader/doc.go
@@ -1,28 +1,29 @@
 /*
-Package respreader provides a convenient way to frame read data from devices
+Package respreader provides a convenient way to frame response data from devices
 that use
-prompt/response protocols such as Modbus (and other RS485 protocols) and modem
+prompt/response protocols such as Modbus, other RS485 protocols, and modem
 AT commands. The fundamental assumption is a device takes some variable amount of
-time to respond to a request, formats up a packet, and then streams it out the
-serial port. Once the response data starts streaming, any significant gap with
-no data indicates the response is complete. Once this gap is detected, a Read()
-returns with accumulated data.
+time to respond to a prompt, formats up a response, and then streams it out the
+serial port. Once the response data starts streaming, any significant gap in the
+response with
+no data indicates the response is complete. A Read() blocks until it detects this
+"gap" or the overall timeout is reached, and then returns with accumulated data.
 
 This method of framing a response has the following advantages:
 
 1) minimizes the wasted timing waiting for a response to the chunkTimeout defined
-below. More simplistic implementations may take the worste case response time
-for all packets and simply wait that amount of time for the response to come.
-This works, but the bus is tied up during the wait that could be used for
-more packets.
+below. More simplistic implementations often take the worst case response time
+for all packets and simply wait that amount of time for the response to arrive.
+This works, but the bus is tied up during this worst case wait that could be used for
+sending the next packet.
 
 2) It is simple in that you don't have to parse the response on the fly to determine
-when it is complete.
+when it is complete, yet it can detect the end of a response fairly quickly.
 
 The obvious disadvantage of this method of framing is that the device may insert a
 significant delay in sending the response that will cause the reader to think the
 resonse is complete. As long as this delay is still significantly shorter than
-the overall response time, it still can work fairly well. Some experiementation may
+the overall response time, it can still work fairly well. Some experiementation may
 be required to optimize the chunkTimeout setting.
 
 Example using a serial port:
