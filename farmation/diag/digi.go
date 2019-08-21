ccfd43b0afe96bb433a62fa662f45d70917696ae
Cliff Brake <cbrake@bec-systems.com>
2019-08-21 15:42:36 -0400

    change to use respreader package

--- farmation/diag/digi.go
+++ farmation/diag/digi.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/simpleiot/simpleiot/network"
+	"github.com/simpleiot/simpleiot/respreader"
 )
 
 // Flush any data from port
@@ -18,7 +18,7 @@ func Flush(port io.Reader) {
 
 // DigiCheckAt puts Digi radio and command mode and executes AT command
 func DigiCheckAt(port io.ReadWriter) error {
-	port = network.NewResponseReadWriter(port, 1500*time.Millisecond,
+	port = respreader.NewResponseReadWriter(port, 1500*time.Millisecond,
 		50*time.Millisecond)
 
 	commandMode := "+++"
