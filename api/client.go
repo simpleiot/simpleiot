3efa26137917036eb30ba0ebbf6a0200659c2342
Cliff Brake <cbrake@bec-systems.com>
2019-09-05 11:46:41 -0400

    add debug to send samples

--- api/client.go
+++ api/client.go
@@ -13,13 +13,17 @@ import (
 
 // NewSendSamples returns a function that can be used to send samples
 // to a SimpleIoT portal instance
-func NewSendSamples(portalURL string) func(string, []data.Sample) error {
+func NewSendSamples(portalURL string, debug bool) func(string, []data.Sample) error {
 	return func(id string, samples []data.Sample) error {
 		sampleURL := portalURL + "/v1/devices/" + id + "/samples"
 
 		tempJSON, err := json.Marshal(samples)
 		if err != nil {
 			log.Println("Error encoding temp: ", err)
+		}
+
+		if debug {
+			log.Println("Sending samples: ", tempJSON)
 		}
 
 		resp, err := http.Post(sampleURL, "application/json", bytes.NewBuffer(tempJSON))
