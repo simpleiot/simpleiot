e2b1d9c3e732be60b971daf278469fc9d342ef52
Cliff Brake <cbrake@bec-systems.com>
2019-09-07 13:03:45 -0400

    add support for getting modem signal/operator, etc

--- network/modem.go
+++ network/modem.go
@@ -1,33 +1,66 @@
 package network
 
 import (
-	"errors"
 	"fmt"
 	"io"
 	"os/exec"
 	"regexp"
 	"strconv"
 	"strings"
+	"time"
 
+	"github.com/cbrake/go-serial/serial"
 	"github.com/simpleiot/simpleiot/file"
+	"github.com/simpleiot/simpleiot/respreader"
 )
 
 // Modem is an interface that always reports detected/connected
 type Modem struct {
-	iface      string
-	chatScript string
-	reset      func() error
-	atCmdPort  io.ReadWriter
-	debug      bool
+	iface         string
+	chatScript    string
+	reset         func() error
+	atCmdPortName string
+	atCmdPort     io.ReadWriteCloser
+	debug         bool
 }
 
 // NewModem constructor
-func NewModem(chatScript string, reset func() error) *Modem {
-	return &Modem{
-		iface:      "ppp0",
-		chatScript: chatScript,
-		reset:      reset,
+func NewModem(chatScript string, atCmdPortName string, reset func() error, debug bool) *Modem {
+	ret := &Modem{
+		iface:         "ppp0",
+		chatScript:    chatScript,
+		reset:         reset,
+		atCmdPortName: atCmdPortName,
+		debug:         debug,
 	}
+
+	return ret
+}
+
+func (m *Modem) openCmdPort() error {
+	if m.atCmdPort != nil {
+		return nil
+	}
+
+	options := serial.OpenOptions{
+		PortName:          m.atCmdPortName,
+		BaudRate:          115200,
+		DataBits:          8,
+		StopBits:          1,
+		MinimumReadSize:   1,
+		RTSCTSFlowControl: true,
+	}
+
+	port, err := serial.Open(options)
+
+	if err != nil {
+		return err
+	}
+
+	m.atCmdPort = respreader.NewResponseReadWriteCloser(port, 10*time.Second,
+		50*time.Millisecond)
+
+	return nil
 }
 
 // Desc returns description
@@ -38,6 +71,10 @@ func (m *Modem) Desc() string {
 // cmd send a command to modem and read response
 // retry 3 times
 func (m *Modem) cmd(cmd string) (string, error) {
+	if err := m.openCmdPort(); err != nil {
+		return "", err
+	}
+
 	var err error
 	for try := 0; try < 3; try++ {
 		readString := make([]byte, 100)
@@ -70,7 +107,7 @@ func (m *Modem) cmd(cmd string) (string, error) {
 
 // service, rssi, rsrp, sinr, rsrq
 // +QCSQ: "CAT-M1",-52,-81,195,-10
-var reQcsq = regexp.MustCompile(`\+QCSQ:\s*\"(.+)\",(\d+),(\d+),(\d+),(\d+)`)
+var reQcsq = regexp.MustCompile(`\+QCSQ:\s*"(.+)",(-*\d+),(-*\d+),(\d+),(-*\d+)`)
 
 func (m *Modem) qcsq() (service bool, rssi, rsrp, rsrq int, err error) {
 	var resp string
@@ -79,19 +116,64 @@ func (m *Modem) qcsq() (service bool, rssi, rsrp, rsrq int, err error) {
 		return
 	}
 
-	matches := reQcsq.FindStringSubmatch(resp)
+	found := false
 
-	if len(matches) < 6 {
-		err = errors.New("Error parsing cmd response")
+	for _, line := range strings.Split(string(resp), "\n") {
+
+		matches := reQcsq.FindStringSubmatch(line)
+
+		if len(matches) < 6 {
+			continue
+		}
+
+		found = true
+
+		serviceS := matches[1]
+		rssi, _ = strconv.Atoi(matches[2])
+		rsrq, _ = strconv.Atoi(matches[3])
+		rsrp, _ = strconv.Atoi(matches[5])
+
+		service = serviceS == "CAT-M1"
+	}
+
+	if !found {
+		err = fmt.Errorf("Error parsing QCSQ response: %v", resp)
+	}
+
+	return
+}
+
+// service, rssi, rsrp, sinr, rsrq
+// +QSPN: "CHN-UNICOM","UNICOM","",0,"46001"
+// +QSPN: "Verizon Wireless","VzW","Hologram",0,"311480"
+
+var reQspn = regexp.MustCompile(`\+QSPN:\s*"(.*)","(.*)","(.*)",(\d+),"(.*)"`)
+
+func (m *Modem) qspn() (network string, err error) {
+	var resp string
+	resp, err = m.cmd("AT+QSPN")
+	if err != nil {
 		return
 	}
 
-	serviceS := matches[1]
-	rssi, _ = strconv.Atoi(matches[2])
-	rsrq, _ = strconv.Atoi(matches[4])
-	rsrp, _ = strconv.Atoi(matches[5])
+	found := false
 
-	service = serviceS == "CAT-M1"
+	for _, line := range strings.Split(string(resp), "\n") {
+
+		matches := reQspn.FindStringSubmatch(line)
+
+		if len(matches) < 6 {
+			continue
+		}
+
+		found = true
+
+		network = matches[1]
+	}
+
+	if !found {
+		err = fmt.Errorf("Error parsing QSPN response: %v", resp)
+	}
 
 	return
 }
@@ -101,8 +183,7 @@ func (m *Modem) detected() bool {
 	return file.Exists("/dev/ttyUSB2") && file.Exists("/dev/ttyUSB3")
 }
 
-// Connected returns true if connected
-func (m *Modem) connected() bool {
+func (m *Modem) pppActive() bool {
 	if !m.detected() {
 		return false
 	}
@@ -123,16 +204,37 @@ func (m *Modem) Connect() error {
 
 // GetStatus return interface status
 func (m *Modem) GetStatus() (InterfaceStatus, error) {
+	var retError error
 	ip, _ := GetIP(m.iface)
+
+	service, rssi, rsrp, rsrq, err := m.qcsq()
+	if err != nil {
+		retError = err
+	}
+
+	network, err := m.qspn()
+	if err != nil {
+		retError = err
+	}
+
 	return InterfaceStatus{
 		Detected:  m.detected(),
-		Connected: m.connected(),
+		Connected: m.pppActive() && service,
+		Operator:  network,
 		IP:        ip,
-	}, nil
+		Signal:    rssi,
+		Rsrp:      rsrp,
+		Rsrq:      rsrq,
+	}, retError
 }
 
 // Reset stub
 func (m *Modem) Reset() error {
+	if m.atCmdPort != nil {
+		m.atCmdPort.Close()
+		m.atCmdPort = nil
+	}
+
 	exec.Command("poff").Run()
 	return m.reset()
 }
