f1e420ca4d09ca771916a07465d1d27d151898f8
Cliff Brake <cbrake@bec-systems.com>
2019-10-07 22:31:25 -0400

    improve tests

--- cmd/serial-close-test/main.go
+++ cmd/serial-close-test/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"io"
+	"log"
 	"os"
+	"os/exec"
+	"sync"
 	"time"
 
 	"github.com/cbrake/go-serial/serial"
@@ -12,17 +14,15 @@ import (
 // the below test illustrates out the goroutine in the reader will close if you close
 // the underlying serial port descriptor
 func main() {
-	fmt.Println("=============================")
-	fmt.Println("Testing serial port close")
+	log.Println("=============================")
+	log.Println("Testing serial port close")
 
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: serial-close-test /dev/ttyUSBx")
+		log.Println("Usage: serial-close-test /dev/ttyUSBx")
 		os.Exit(-1)
 	}
 
 	port := os.Args[1]
-
-	done := make(chan bool)
 
 	options := serial.OpenOptions{
 		PortName:              port,
@@ -35,35 +35,79 @@ func main() {
 
 	fread, err := serial.Open(options)
 	if err != nil {
-		fmt.Println("Error opening serial port: ", err)
-		os.Exit(-1)
+		log.Fatal("Error opening serial port: ", err)
 	}
 
-	go func(readCnt chan bool) {
+	var wg sync.WaitGroup
+
+	go func() {
+		wg.Add(1)
 		rdata := make([]byte, 128)
 		for {
-			fmt.Println("calling read")
+			log.Println("calling read on serial port")
 			c, err := fread.Read(rdata)
 			if err == io.EOF {
-				fmt.Println("Reader returned EOF, yeah, this is good!")
+				log.Println("serial read returned EOF, yeah, this is good!")
 				break
 			}
 			if err != nil {
-				fmt.Println("Read error: ", err)
+				log.Println("serial read error: ", err)
 			}
-			fmt.Println("read count: ", c)
+			log.Println("read count: ", c)
+		}
+		wg.Done()
+	}()
+
+	// Fifo test
+	os.Remove("fifo")
+	err = exec.Command("mkfifo", "fifo").Run()
+	if err != nil {
+		log.Fatal("mkfifo failed: ", err)
+	}
+
+	var fFifoRead io.ReadWriteCloser
+
+	go func() {
+		wg.Add(1)
+		fFifoRead, err = os.OpenFile("fifo", os.O_RDONLY, 0600)
+		if err != nil {
+			log.Fatal("Error opening fifo for read: ", err)
 		}
 
-		done <- true
-	}(done)
+		rdata := make([]byte, 128)
+		for {
+			log.Println("call read on fifo")
+			c, err := fFifoRead.Read(rdata)
+			log.Printf("read %v bytes from fifo", c)
+			if err == io.EOF {
+				log.Println("fifo read error: ", err)
+				break
+			}
+
+			if c == 0 {
+				log.Println("read 0 bytes from fifo, done")
+				break
+			}
+		}
+		wg.Done()
+	}()
 
 	time.Sleep(500 * time.Millisecond)
 
+	fFifoWrite, err := os.OpenFile("fifo", os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal("Error opening fifo for write: ", err)
+	}
+	fFifoWrite.Write([]byte("hi there"))
+
 	// the following should unblock the above read
-	fmt.Println("Closing read file")
+	// but it does not
+	log.Println("Closing serial and fifo read files")
 	fread.Close()
+	fFifoRead.Close()
 
-	<-done
+	log.Println("Waiting for reads to unblock ....")
+	wg.Wait()
 
-	fmt.Println("test all done")
+	log.Println("test all done, both reads were unblocked")
 }
