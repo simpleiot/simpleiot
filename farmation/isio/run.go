a9b2438064f85d633334401af378432e14d5d002
Collin Brake <collin@bec-systems.com>
2020-01-24 11:54:48 -0500

    slowed down polling rate for gpios

--- farmation/isio/run.go
+++ farmation/isio/run.go
@@ -39,7 +39,7 @@ func Run(in, out chan interface{}, configInit isdata.Config, stateInit isdata.St
 		}
 	}
 
-	gpioReadTicker := time.NewTicker(50 * time.Millisecond) // ticker to read gpio's
+	gpioReadTicker := time.NewTicker(500 * time.Millisecond) // ticker to read gpio's
 	panelSenseTicker := time.NewTicker(10 * time.Second)
 
 	if runtime.GOARCH != "arm" {
