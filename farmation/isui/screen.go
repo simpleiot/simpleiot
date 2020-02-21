8f640cdbb54a8b9b60d6dea91f180a8db3eb2821
Collin Brake <collinmbrake@gmail.com>
2020-02-21 14:31:11 -0500

    take to pump mode screen if arming in "OFF" mode

--- farmation/isui/screen.go
+++ farmation/isui/screen.go
@@ -55,6 +55,7 @@ type Screens struct {
 	dialog        *DialogScreen
 	dialogArmReq  *DialogArmReqScreen
 	state         *isdata.State
+	config        *isdata.Config
 }
 
 // Add a new screen
@@ -65,7 +66,8 @@ func (s *Screens) Add(ID ScreenID, screen Widget) {
 // NewScreens initializes all screens
 func NewScreens(state *isdata.State, config *isdata.Config, db *isdb.IsDb) *Screens {
 	ret := &Screens{
-		state: state,
+		state:  state,
+		config: config,
 	}
 
 	ret.dialog = NewDialogScreen()
@@ -162,11 +164,17 @@ func (s *Screens) Key(key isdata.Key) (ScreenID, interface{}, bool) {
 
 		// Take user directly to a screen that needs attention
 		// when the dialog is closed
-		if currentDialog.ID == isdata.DialogArm {
+		switch currentDialog.ID {
+		case isdata.DialogArm:
 			switch currentDialog.Message {
 			case "Cannot arm in Monitor \nOnly mode, please switch \nmodes":
 				s.switchScreen(ScreenIDOpMode1)
 			case "Injector Command \nInput not selected, please \nselect before arming":
+				s.switchScreen(ScreenIDPumpMode)
+			}
+		case isdata.DialogArmReq:
+			if s.state.InputInjector == isdata.InputStateOff &&
+				s.config.UserPumpMode == isdata.UserPumpModeOff {
 				s.switchScreen(ScreenIDPumpMode)
 			}
 		}
