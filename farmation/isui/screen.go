d445af55593a0cb9fb7d87a3f3aba6cff67452d1
Collin Brake <collinmbrake@gmail.com>
2020-03-04 14:54:43 -0500

    bug fix for dialog back key handling

--- farmation/isui/screen.go
+++ farmation/isui/screen.go
@@ -148,7 +148,9 @@ func (s *Screens) Key(key isdata.Key) (ScreenID, interface{}, bool) {
 	// from the dialog
 	if currentDialog != nil &&
 		// when the back key is pressed
-		(key == isdata.KeySK1Release || key == isdata.KeySK1Hold) {
+		// we have to check for KeySK1 here or it will not trigger this on the initial
+		// press and will get handled by the screen, not the dialog
+		(key == isdata.KeySK1Release || key == isdata.KeySK1 || key == isdata.KeySK1Hold) {
 
 		// Take user directly to a screen that needs attention
 		switch currentDialog.ID {
