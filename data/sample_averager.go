d36ffde4f75c48d0fedae71e7f1d87750f1b431b
Collin Brake <collinmbrake@gmail.com>
2020-04-18 21:19:20 -0400

    export all sample averager struct fields to serialize with gob

--- data/sample_averager.go
+++ data/sample_averager.go
@@ -7,69 +7,69 @@ import (
 // SampleAverager accumulates samples, and averages them. The average can
 // be reset.
 type SampleAverager struct {
-	total      float64
-	count      int
-	min        float64
-	max        float64
-	sampleType string
-	sampleTime time.Time
+	Total      float64
+	Count      int
+	Min        float64
+	Max        float64
+	SampleType string
+	SampleTime time.Time
 }
 
 // NewSampleAverager initializes and returns an averager
 func NewSampleAverager(sampleType string) *SampleAverager {
 	return &SampleAverager{
-		sampleType: sampleType,
+		SampleType: sampleType,
 	}
 }
 
 func (sa *SampleAverager) GetSampleType() string {
-	return sa.sampleType
+	return sa.SampleType
 }
 
 // AddSample takes a sample, and adds it to the total
 func (sa *SampleAverager) AddSample(s Sample) {
 	// avg sample timestamp is set to last sample time
-	if s.Time.After(sa.sampleTime) {
-		sa.sampleTime = s.Time
+	if s.Time.After(sa.SampleTime) {
+		sa.SampleTime = s.Time
 	}
 
 	// update statistical values.
-	sa.total += s.Value
-	sa.count++
+	sa.Total += s.Value
+	sa.Count++
 	// min
-	if sa.min == 0 {
-		sa.min = s.Min
-	} else if s.Min < sa.min {
-		sa.min = s.Min
+	if sa.Min == 0 {
+		sa.Min = s.Min
+	} else if s.Min < sa.Min {
+		sa.Min = s.Min
 	}
 	// max
-	if s.Max > sa.max {
-		sa.max = s.Max
+	if s.Max > sa.Max {
+		sa.Max = s.Max
 	}
 }
 
 // ResetAverage sets the accumulated total to zero
 func (sa *SampleAverager) ResetAverage() {
-	sa.total = 0
-	sa.count = 0
-	sa.min = 0
-	sa.max = 0
+	sa.Total = 0
+	sa.Count = 0
+	sa.Min = 0
+	sa.Max = 0
 }
 
 // GetAverage returns the average of the accumulated samples
 func (sa *SampleAverager) GetAverage() Sample {
 	var value float64
-	if sa.count == 0 {
+	if sa.Count == 0 {
 		value = 0
 	} else {
-		value = sa.total / float64(sa.count)
+		value = sa.Total / float64(sa.Count)
 	}
 
 	return Sample{
-		Type:  sa.sampleType,
-		Time:  sa.sampleTime,
+		Type:  sa.SampleType,
+		Time:  sa.SampleTime,
 		Value: value,
-		Min:   sa.min,
-		Max:   sa.max,
+		Min:   sa.Min,
+		Max:   sa.Max,
 	}
 }
