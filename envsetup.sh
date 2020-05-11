cf27301fb0679f6434b7927aa95dd6bc3328acab
Cliff Brake <cbrake@bec-systems.com>
2020-05-11 17:53:13 -0400

    fix passing in params to siot_watch

--- envsetup.sh
+++ envsetup.sh
@@ -104,7 +104,7 @@ find_src_files() {
 }
 
 siot_watch() {
-  find_src_files | entr -r /bin/sh -c ". ./envsetup.sh; siot_run 2 -debugHttp" "$@" 
+  find_src_files | entr -r /bin/sh -c ". ./envsetup.sh; siot_run 2 -debugHttp -sim $@" 
 }
 
 siot_run_device_sim() {
