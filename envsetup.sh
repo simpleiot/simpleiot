316dc6d84806afdc139f1c9a7c87d75980a534e0
Cliff Brake <cbrake@bec-systems.com>
2020-05-18 15:16:40 -0400

    ui is running again from frontend dir

--- envsetup.sh
+++ envsetup.sh
@@ -43,20 +43,18 @@ siot_setup() {
 }
 
 siot_build_frontend() {
-  rm -f "frontend$1/output"/*
-  if [ "$1" = "2" ]; then
-    (cd "frontend$1" && npx elm-spa build) || return 1
-  fi
-  (cd "frontend$1" && npx elm make --debug src/Main.elm --output=output/elm.js) || return 1
-  cp "frontend$1/public"/* "frontend$1/output/" || return 1
-  cp "frontend$1/public/index$1.html" "frontend$1/output/index.html" || return 1
-  cp docs/simple-iot-app-logo.png "frontend$1/output/" || return 1
+  rm -f "frontend/output"/*
+  (cd "frontend" && npx elm-spa build) || return 1
+  (cd "frontend" && npx elm make --debug src/Main.elm --output=output/elm.js) || return 1
+  cp "frontend/public"/* "frontend/output/" || return 1
+  cp "frontend/public/index.html" "frontend/output/index.html" || return 1
+  cp docs/simple-iot-app-logo.png "frontend/output/" || return 1
   return 0
 }
 
 siot_build_assets() {
   mkdir -p assets/frontend || return 1
-  genesis -C "frontend$1/output" -pkg frontend \
+  genesis -C "frontend/output" -pkg frontend \
     index.html \
     elm.js \
     main.js \
@@ -69,14 +67,14 @@ siot_build_assets() {
 }
 
 siot_build_dependencies() {
-  siot_build_frontend "$1" || return 1
-  siot_build_assets "$1" || return 1
+  siot_build_frontend || return 1
+  siot_build_assets || return 1
   return 0
 }
 
 # the following can be used to build v2 of the frontend: siot_build 2
 siot_build() {
-  siot_build_dependencies "$1" || return 1
+  siot_build_dependencies || return 1
   go build -o siot cmd/siot/main.go || return 1
   return 0
 }
@@ -89,24 +87,18 @@ siot_deploy() {
 
 siot_run() {
   echo "run args: $@"
-  frontend_version=""
-  if [ "$1" = "2" ]; then
-    frontend_version=2
-    shift
-  fi
-
-  siot_build_dependencies "$frontend_version" || return 1
+  siot_build_dependencies || return 1
   go run cmd/siot/main.go "$@" || return 1
   return 0
 }
 
 find_src_files() {
-  find . -not \( -path ./frontend2/elm-stuff -prune \) -not \( -path ./assets -prune \) -name "*.go" -o -name "*.elm"
+  find . -not \( -path ./frontend/elm-stuff -prune \) -not \( -path ./assets -prune \) -name "*.go" -o -name "*.elm"
 }
 
 siot_watch() {
   echo "watch args: $@"
-  cmd=". ./envsetup.sh; siot_run 2 $@"
+  cmd=". ./envsetup.sh; siot_run $@"
   find_src_files | entr -r /bin/sh -c "$cmd"
 }
 
@@ -130,8 +122,8 @@ check_go_format() {
 # please run the following before pushing -- best if your editor can be set up
 # to do this automatically.
 siot_test() {
-  siot_build_dependencies 2 || return 1
-  (cd frontend2 && npx elm-analyse || return 1) || return 1
+  siot_build_dependencies || return 1
+  (cd frontend && npx elm-analyse || return 1) || return 1
   #gofmt -l ./... || return 1
   go test "$@" ./... || return 1
   golint -set_exit_status ./... || return 1
