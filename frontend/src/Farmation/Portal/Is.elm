12054fdd9865455bc9205333ff18616191cb6657
Cliff Brake <cbrake@bec-systems.com>
2020-05-20 11:34:34 -0400

    start of IS view

---
+++ frontend/src/Farmation/Portal/Is.elm
@@ -0,0 +1,188 @@
+module Farmation.Portal.Is exposing (view)
+
+import Data.Device
+import Data.Sample exposing (Sample)
+import Element exposing (..)
+import Round
+import Utils.Styles as Styles
+
+
+type alias InjectorSentry =
+    { inputWaterOn : Bool
+    , inputIrrigator : Bool
+    , inputInjector : Bool
+    , armed : Bool
+    , outputShutdown : Bool
+    , outputInjector : Bool
+    , flowRate : Float
+    , currentTankVolume : Float
+    , tankCapacity : Float
+    , pressureMin : Float
+    , pressureMax : Float
+    , flowRateTarget : Float
+    , flowWindowLow : Float
+    , flowWindowHigh : Float
+    }
+
+
+view : Data.Device.Device -> Element msg
+view dev =
+    let
+        is =
+            deviceToInjectorSentry dev
+
+        inputInj =
+            if is.inputInjector then
+                image [] { src = "public/Injector.png", description = "injector" }
+
+            else
+                Element.none
+
+        inputWater =
+            if is.inputWaterOn then
+                image [] { src = "public/WaterOn.png", description = "water on" }
+
+            else
+                Element.none
+
+        inputIrr =
+            if is.inputIrrigator then
+                image [] { src = "public/Irrigator.png", description = "irrigator" }
+
+            else
+                Element.none
+
+        flow =
+            text (Round.round 1 is.flowRate ++ " GPH")
+
+        armed =
+            if is.armed then
+                image [] { src = "public/Armed.png", description = "armed" }
+
+            else
+                Element.none
+
+        outputInj =
+            if is.outputInjector then
+                image [] { src = "public/Injector.png", description = "injector" }
+
+            else
+                Element.none
+
+        outputShutdown =
+            if is.outputShutdown then
+                image [] { src = "public/Shutdown.png", description = "shutdown" }
+
+            else
+                Element.none
+    in
+    column [ spacing 16 ]
+        [ row []
+            [ Styles.h3 [] <| text <| "(" ++ dev.id ++ ")"
+            ]
+        , wrappedRow []
+            [ inputInj
+            , space
+            , inputWater
+            , space
+            , inputIrr
+            , space
+            , flow
+            , space
+            , armed
+            , space
+            , outputInj
+            , space
+            , outputShutdown
+            ]
+        , text ("Min Pressure: " ++ Round.round 0 is.pressureMin)
+        , text ("Max Pressure: " ++ Round.round 0 is.pressureMax)
+        , text
+            ("Tank Level: "
+                ++ Round.round 0
+                    is.currentTankVolume
+                ++ " of "
+                ++ Round.round 0 is.tankCapacity
+                ++ " gal"
+            )
+        , text ("Target Flow: " ++ Round.round 0 is.flowRateTarget)
+        , text
+            ("Target Flow Window: "
+                ++ Round.round 0 is.flowWindowLow
+                ++ " to "
+                ++ Round.round 0 is.flowWindowHigh
+            )
+        ]
+
+
+space : Element msg
+space =
+    text "  "
+
+
+deviceToInjectorSentry : Data.Device.Device -> InjectorSentry
+deviceToInjectorSentry device =
+    let
+        is =
+            InjectorSentry False False False False False False 0 0 0 0 0 0 0 0
+    in
+    isApplyIos is device.state.ios
+
+
+isApplyIos : InjectorSentry -> List Sample -> InjectorSentry
+isApplyIos is ios =
+    case ios of
+        x :: xs ->
+            case x.sType of
+                "armed" ->
+                    isApplyIos { is | armed = floatToBool x.value } xs
+
+                "inputIrrigator" ->
+                    isApplyIos { is | inputIrrigator = floatToBool x.value } xs
+
+                "inputWaterOn" ->
+                    isApplyIos { is | inputWaterOn = floatToBool x.value } xs
+
+                "inputInjector" ->
+                    isApplyIos { is | inputInjector = floatToBool x.value } xs
+
+                "gpioRelayInjectorEn" ->
+                    isApplyIos { is | outputInjector = floatToBool x.value } xs
+
+                "gpioRelayShutdownEn" ->
+                    isApplyIos { is | outputShutdown = floatToBool x.value } xs
+
+                "flowRate" ->
+                    isApplyIos { is | flowRate = x.value } xs
+
+                "pressureMin" ->
+                    isApplyIos { is | pressureMin = x.value } xs
+
+                "pressureMax" ->
+                    isApplyIos { is | pressureMax = x.value } xs
+
+                "currentTankVolume" ->
+                    isApplyIos { is | currentTankVolume = x.value } xs
+
+                "tankCapacity" ->
+                    isApplyIos { is | tankCapacity = x.value } xs
+
+                "flowRateTarget" ->
+                    isApplyIos { is | flowRateTarget = x.value } xs
+
+                "flowWindowLow" ->
+                    isApplyIos { is | flowWindowLow = x.value } xs
+
+                "flowWindowHigh" ->
+                    isApplyIos { is | flowWindowHigh = x.value } xs
+
+                _ ->
+                    isApplyIos is xs
+
+        [] ->
+            is
+
+
+floatToBool : Float -> Bool
+floatToBool input =
+    input /= 0
