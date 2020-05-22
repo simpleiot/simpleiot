a4403cc55f1be4bb351b21c66c088099efadfbd3
Cliff Brake <cbrake@bec-systems.com>
2020-05-22 14:26:07 -0400

    make sending data a little smarter

--- farmation/isnetwork/run.go
+++ farmation/isnetwork/run.go
@@ -2,7 +2,6 @@ package isnetwork
 
 import (
 	"log"
-	"math"
 	"runtime"
 	"strconv"
 	"time"
@@ -32,6 +31,83 @@ func input2Float(in isdata.InputState) float64 {
 	return 0
 }
 
+func getConfigSamples(config *isdata.Config) []data.Sample {
+	windowHigh, windowLow := config.CalculateFlowWindow()
+
+	return []data.Sample{
+		{
+			Type:  "armed",
+			Value: bool2Float(config.Arm),
+		},
+		{
+			Type:  "flowRateTarget",
+			Value: config.FlowRateTarget,
+		},
+		{
+			Type:  "flowWindowLow",
+			Value: windowLow,
+		},
+		{
+			Type:  "flowWindowHigh",
+			Value: windowHigh,
+		},
+		{
+			Type:  "tankCapacity",
+			Value: float64(config.TankCapacity),
+		},
+	}
+}
+
+func getDigIoSamples(state *isdata.State) []data.Sample {
+	return []data.Sample{
+		{
+			Type:  "inputWaterOn",
+			Value: input2Float(state.InputWaterOn),
+		},
+		{
+			Type:  "inputIrrigator",
+			Value: input2Float(state.InputIrrigator),
+		},
+		{
+			Type:  "inputInjector",
+			Value: input2Float(state.InputInjector),
+		},
+		{
+			Type:  "gpioRelayInjectorEn",
+			Value: bool2Float(state.GpioRelayInjectorEn),
+		},
+		{
+			Type:  "gpioShutdownEn",
+			Value: bool2Float(state.GpioRelayShutdownEn),
+		},
+	}
+}
+
+func getAnalogSamples(state *isdata.State) []data.Sample {
+	return []data.Sample{
+		{
+			Type:  "flowRate",
+			Value: state.FlowRate,
+		},
+		{
+			Type:  "currentTankVolume",
+			Value: state.CurrentTankVolume,
+		},
+		{
+			Type:  "avgFlowRate",
+			Value: state.FlowAverager.GetAverage().Value,
+		},
+		{
+			Type:  "pressureMin",
+			Value: state.PressureMin,
+		},
+		{
+			Type:  "pressureMax",
+			Value: state.PressureMax,
+		},
+	}
+}
+
 // Run is the entry point for the isnetwork subsystem
 func Run(in, out chan interface{}, configIn isdata.Config,
 	stateIn isdata.State, portal string, debugPortal bool) {
@@ -49,10 +125,17 @@ func Run(in, out chan interface{}, configIn isdata.Config,
 	getCmdAPI := api.NewGetCmd(portal, state.SerialNumber, time.Second*10, debugPortal)
 	setVersionAPI := api.NewSetVersion(portal, state.SerialNumber, time.Second*10, debugPortal)
 
+	whenChangedSamplesFilter := api.NewSampleFilter(0, 15*time.Minute)
+	analogSamplesFilter := api.NewSampleFilter(30*time.Second, 15*time.Minute)
+
 	versionSent := false
 
 	sendSamples := func(samples []data.Sample) error {
 		if len(samples) <= 0 {
+			return nil
+		}
+
+		if state.SerialNumber == "" || portal == "" {
 			return nil
 		}
 
@@ -110,50 +193,8 @@ func Run(in, out chan interface{}, configIn isdata.Config,
 			return
 		}
 
-		windowHigh, windowLow := config.CalculateFlowWindow()
-
-		samples := []data.Sample{
-			{
-				Type:  "armed",
-				Value: bool2Float(config.Arm),
-			},
-			{
-				Type:  "flowRateTarget",
-				Value: config.FlowRateTarget,
-			},
-			{
-				Type:  "flowWindowLow",
-				Value: windowLow,
-			},
-			{
-				Type:  "flowWindowHigh",
-				Value: windowHigh,
-			},
-			{
-				Type:  "tankCapacity",
-				Value: float64(config.TankCapacity),
-			},
-			{
-				Type:  "inputWaterOn",
-				Value: input2Float(state.InputWaterOn),
-			},
-			{
-				Type:  "inputIrrigator",
-				Value: input2Float(state.InputIrrigator),
-			},
-			{
-				Type:  "inputInjector",
-				Value: input2Float(state.InputInjector),
-			},
-			{
-				Type:  "gpioRelayInjectorEn",
-				Value: bool2Float(state.GpioRelayInjectorEn),
-			},
-			{
-				Type:  "gpioShutdownEn",
-				Value: bool2Float(state.GpioRelayShutdownEn),
-			},
-		}
+		samples := whenChangedSamplesFilter.Add(getConfigSamples(&config))
+		samples = append(samples, analogSamplesFilter.Add(getDigIoSamples(&state))...)
 
 		err := sendSamples(samples)
 		if err == nil {
@@ -162,7 +203,6 @@ func Run(in, out chan interface{}, configIn isdata.Config,
 	}
 
 	manageTicker := time.NewTicker(time.Second * 10)
-	sendPortal := time.NewTicker(time.Minute * 10)
 	pollPortal := time.NewTicker(time.Minute)
 
 	if runtime.GOARCH != "arm" {
@@ -178,133 +218,34 @@ func Run(in, out chan interface{}, configIn isdata.Config,
 	)
 
 	if state.SerialNumber == "" {
-		log.Println("IS Serial is not set, not sending data to portal")
-		sendPortal.Stop()
+		log.Println("IS Serial is not set, not polling portal")
 		pollPortal.Stop()
 	}
 
 	if portal == "" {
-		log.Println("Portal URL is not set, not sending data to portal")
-		sendPortal.Stop()
+		log.Println("Portal URL is not set, not polling portal")
 		pollPortal.Stop()
 	}
 
 	sendInitialDigitalData()
-
-	lastReportedFlow := 0.0
-	lastReportedTankVolume := 0.0
 
 	for {
 		select {
 		case m := <-in:
 			switch m := m.(type) {
 			case isdata.Config:
-				samples := []data.Sample{}
-				if config.Arm != m.Arm {
-					samples = append(samples,
-						data.Sample{
-							Type:  "armed",
-							Value: bool2Float(m.Arm),
-						})
-				}
-
-				if config.FlowRateTarget != m.FlowRateTarget {
-					samples = append(samples,
-						data.Sample{
-							Type:  "flowRateTarget",
-							Value: m.FlowRateTarget,
-						})
-				}
-
-				windowHigh, windowLow := config.CalculateFlowWindow()
-				windowHighN, windowLowN := config.CalculateFlowWindow()
-
-				if windowLow != windowLowN {
-					samples = append(samples,
-						data.Sample{
-							Type:  "flowWindowLow",
-							Value: windowLowN,
-						})
-				}
-
-				if windowHigh != windowHighN {
-					samples = append(samples,
-						data.Sample{
-							Type:  "flowWindowHigh",
-							Value: windowHighN,
-						})
-				}
-
-				sendSamples(samples)
-
 				config = m
+				samples := whenChangedSamplesFilter.Add(getConfigSamples(&config))
+				sendSamples(samples)
 
 			case isdata.State:
-				samples := []data.Sample{}
-				if state.InputWaterOn != m.InputWaterOn {
-					samples = append(samples,
-						data.Sample{
-							Type:  "inputWaterOn",
-							Value: input2Float(m.InputWaterOn),
-						})
-				}
+				state = m
 
-				if state.InputIrrigator != m.InputIrrigator {
-					samples = append(samples,
-						data.Sample{
-							Type:  "inputIrrigator",
-							Value: input2Float(m.InputIrrigator),
-						})
-				}
-
-				if state.InputInjector != m.InputInjector {
-					samples = append(samples,
-						data.Sample{
-							Type:  "inputInjector",
-							Value: input2Float(m.InputInjector),
-						})
-				}
-
-				if state.GpioRelayInjectorEn != m.GpioRelayInjectorEn {
-					samples = append(samples,
-						data.Sample{
-							Type:  "gpioRelayInjectorEn",
-							Value: bool2Float(m.GpioRelayInjectorEn),
-						})
-				}
-
-				if state.GpioRelayShutdownEn != m.GpioRelayShutdownEn {
-					samples = append(samples,
-						data.Sample{
-							Type:  "gpioShutdownEn",
-							Value: bool2Float(m.GpioRelayShutdownEn),
-						})
-				}
-
-				if math.Abs(lastReportedFlow-m.FlowRate) > 5 {
-					samples = append(samples,
-						data.Sample{
-							Type:  "flowRate",
-							Value: m.FlowRate,
-						})
-
-					lastReportedFlow = m.FlowRate
-
-				}
-
-				if math.Abs(lastReportedTankVolume-m.CurrentTankVolume) > 10 {
-					samples = append(samples,
-						data.Sample{
-							Type:  "currentTankVolume",
-							Value: m.CurrentTankVolume,
-						})
-
-					lastReportedTankVolume = m.CurrentTankVolume
-				}
-
+				samples := whenChangedSamplesFilter.Add(getDigIoSamples(&state))
 				sendSamples(samples)
 
-				state = m
+				samples = analogSamplesFilter.Add(getAnalogSamples(&state))
+				sendSamples(samples)
 
 			case data.Sample:
 				switch m.Type {
@@ -365,6 +306,8 @@ func Run(in, out chan interface{}, configIn isdata.Config,
 				continue
 			}
 
+			sendInitialDigitalData()
+
 			cmd, err := getCmdAPI()
 
 			if err != nil {
@@ -389,41 +332,6 @@ func Run(in, out chan interface{}, configIn isdata.Config,
 				}
 
 				versionSent = true
-			}
-
-		case <-sendPortal.C:
-			sendInitialDigitalData()
-			if !interfaceStatus.Connected {
-				continue
-			}
-			samples := []data.Sample{
-				{
-					Type:  "flowRate",
-					Value: state.FlowRate,
-				},
-				{
-					Type:  "currentTankVolume",
-					Value: state.CurrentTankVolume,
-				},
-				{
-					Type:  "avgFlowRate",
-					Value: state.FlowAverager.GetAverage().Value,
-				},
-				{
-					Type:  "pressureMin",
-					Value: state.PressureMin,
-				},
-				{
-					Type:  "pressureMax",
-					Value: state.PressureMax,
-				},
-			}
-
-			if err := sendSamples(samples); err != nil {
-				log.Println("Error sending samples to server: ", err)
-			} else {
-				lastReportedFlow = state.FlowRate
-				lastReportedTankVolume = state.CurrentTankVolume
 			}
 		}
 	}
