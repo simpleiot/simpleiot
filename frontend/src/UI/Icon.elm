9771ce26ca268c2dca70a6c3dbe5cf4c9790f30e
Cliff Brake <cbrake@bec-systems.com>
2020-06-24 17:15:36 -0400

    add cloud and power icons

--- frontend/src/UI/Icon.elm
+++ frontend/src/UI/Icon.elm
@@ -1,6 +1,10 @@
 module UI.Icon exposing
     ( check
+    , cloud
+    , cloudOff
     , plus
+    , power
+    , powerOff
     , userCheck
     , userMinus
     , userPlus
@@ -13,11 +17,14 @@ import Element.Background as Background
 import Element.Border as Border
 import Element.Input as Input
 import FeatherIcons
+import Global exposing (Msg)
+import Svg exposing (Svg, svg)
+import Svg.Attributes exposing (..)
 import UI.Style as Style
 
 
-i : FeatherIcons.Icon -> msg -> Element msg
-i icon msg =
+button : FeatherIcons.Icon -> msg -> Element msg
+button iconIn msg =
     Input.button
         [ padding 5
         , Border.rounded 50
@@ -32,40 +39,73 @@ i icon msg =
         { onPress = Just msg
         , label =
             html
-                (FeatherIcons.toHtml [] icon)
+                (FeatherIcons.toHtml [] iconIn)
         }
+
+
+icon : FeatherIcons.Icon -> Element msg
+icon iconIn =
+    html <| FeatherIcons.toHtml [] iconIn
 
 
 x : msg -> Element msg
 x msg =
-    i FeatherIcons.x msg
+    button FeatherIcons.x msg
 
 
 check : msg -> Element msg
 check msg =
-    i FeatherIcons.check msg
+    button FeatherIcons.check msg
 
 
 plus : msg -> Element msg
 plus msg =
-    i FeatherIcons.plus msg
+    button FeatherIcons.plus msg
 
 
 userPlus : msg -> Element msg
 userPlus msg =
-    i FeatherIcons.userPlus msg
+    button FeatherIcons.userPlus msg
 
 
 userX : msg -> Element msg
 userX msg =
-    i FeatherIcons.userX msg
+    button FeatherIcons.userX msg
 
 
 userMinus : msg -> Element msg
 userMinus msg =
-    i FeatherIcons.userMinus msg
+    button FeatherIcons.userMinus msg
 
 
 userCheck : msg -> Element msg
 userCheck msg =
-    i FeatherIcons.userCheck msg
+    button FeatherIcons.userCheck msg
+
+
+cloudOff : Element msg
+cloudOff =
+    icon FeatherIcons.cloudOff
+
+
+cloud : Element msg
+cloud =
+    icon FeatherIcons.cloud
+
+
+power : Element msg
+power =
+    icon FeatherIcons.power
+
+
+
+-- powerOff icon is not finished yet
+
+
+powerOff : Element Msg
+powerOff =
+    [ Svg.path [ d "M18.36 6.64a9 9 0 1 1-12.73 0" ] []
+    , Svg.line [ x1 "12", y1 "2", x2 "12", y2 "12" ] []
+    ]
+        |> FeatherIcons.customIcon
+        |> icon
