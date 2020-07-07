351d86d9ec0db95b3896222ae748882290873b2e
Collin Brake <collinmbrake@gmail.com>
2020-07-07 08:55:46 -0400

    handle case where config version less than zero more robustly

--- farmation/isdata/config.go
+++ farmation/isdata/config.go
@@ -459,35 +459,34 @@ func migration3(c *Config) {
 // Init is used to inialize the config
 func (c *Config) Init(state *State) {
 
-	// Run migrations
+	/*
+		Run migrations
+	*/
 
-	// Check that the DBVersion from the state is not
-	// a bogus value
 	if state.DBConfig.DBVersion < 0 {
-		log.Println("Error running config migrations: bogus "+
-			"database version from the state:",
-			state.DBConfig.DBVersion)
-	} else {
+		log.Println("Error, config version less than zero - " +
+			"resetting to zero.")
+		state.DBConfig.DBVersion = 0
+	}
 
-		// If a migration is added and then removed, adjust
-		// migration number
-		migLen := len(migrations) - 1
-		if state.DBConfig.DBVersion > migLen {
-			log.Printf("Detected removed migration(s) - "+
-				"decreased config version: %v -> %v",
-				state.DBConfig.DBVersion, migLen)
-			state.DBConfig.DBVersion = migLen
-		}
+	// If a migration is added and then removed, adjust
+	// migration number
+	migLen := len(migrations) - 1
+	if state.DBConfig.DBVersion > migLen {
+		log.Printf("Detected removed migration(s) - "+
+			"decreased config version: %v -> %v",
+			state.DBConfig.DBVersion, migLen)
+		state.DBConfig.DBVersion = migLen
+	}
 
-		// Will ONLY run if new migration(s) have been added
-		// and DBVersion is less than the length of migrations
-		for v, mig := range migrations {
+	// Will ONLY run if new migration(s) have been added
+	// and DBVersion is less than the length of migrations
+	for v, mig := range migrations {
 
-			if v > state.DBConfig.DBVersion {
-				mig(c)
-				state.DBConfig.DBVersion = v
-				log.Println("Config migration number", v)
-			}
+		if v > state.DBConfig.DBVersion {
+			mig(c)
+			state.DBConfig.DBVersion = v
+			log.Println("Config migration number", v)
 		}
 	}
 
