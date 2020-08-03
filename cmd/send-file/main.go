d5cc366688e4e74e7a05729e25d366fce862fe9e
Cliff Brake <cbrake@bec-systems.com>
2020-08-03 18:12:00 -0400

    add send file test app

---
+++ cmd/send-file/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"time"
+
+	"github.com/nats-io/nats.go"
+	"github.com/simpleiot/simpleiot/api"
+)
+
+func main() {
+	flagNatsServer := flag.String("natsServer", "nats://localhost:4222", "NATS Server")
+	flagURL := flag.String("url", "", "URL of file to send")
+	flagID := flag.String("id", "1234", "ID of edge device")
+
+	flag.Parse()
+
+	fmt.Println("flagUrl: ", *flagURL)
+	fmt.Println("flagID: ", *flagID)
+
+	if *flagURL == "" || *flagID == "" {
+		log.Println("Error, must provide url and device")
+		flag.Usage()
+		os.Exit(-1)
+	}
+
+	nc, err := nats.Connect(*flagNatsServer,
+		nats.Timeout(10*time.Second),
+		nats.PingInterval(60*2*time.Second),
+		nats.MaxPingsOutstanding(5),
+		nats.ReconnectBufSize(5*1024*1024),
+		nats.MaxReconnects(-1),
+		nats.SetCustomDialer(&net.Dialer{
+			KeepAlive: -1,
+		}),
+		//nats.Token(authToken),
+	)
+	if err != nil {
+		log.Println("Error connecting to NATS server: ", err)
+		os.Exit(-1)
+	}
+
+	log.Println("Connected to server")
+
+	nc.SetErrorHandler(func(_ *nats.Conn, _ *nats.Subscription,
+		err error) {
+		log.Printf("NATS Error: %s\n", err)
+	})
+
+	nc.SetReconnectHandler(func(_ *nats.Conn) {
+		log.Println("NATS Reconnected!")
+	})
+
+	nc.SetDisconnectHandler(func(_ *nats.Conn) {
+		log.Println("NATS Disconnected!")
+	})
+
+	nc.SetClosedHandler(func(_ *nats.Conn) {
+		panic("Connection to NATS is closed!")
+	})
+
+	err = api.NatsSendFileFromHTTP(nc, *flagID, *flagURL)
+
+	if err != nil {
+		log.Println("Error sending file: ", err)
+		os.Exit(-1)
+	}
+
+	log.Println("File sent!")
+}
