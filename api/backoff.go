2daf1c4b31870a5935d2d889d6f46c8e25a39237
Cliff Brake <cbrake@bec-systems.com>
2020-08-04 20:08:17 -0400

    add exp backoff function

---
+++ api/backoff.go
@@ -0,0 +1,18 @@
+package api
+
+import (
+	"math"
+	"math/rand"
+	"time"
+)
+
+// ExpBackoff calculates an exponential time backup to max duration + a random fraction of 1s
+func ExpBackoff(attempts int, max time.Duration) time.Duration {
+	delay := time.Duration(math.Exp2(float64(attempts))) * time.Second
+	if delay > max {
+		delay = max
+	}
+	// randomize a bit
+	delay = delay + time.Duration(rand.Float32()*1000)*time.Millisecond
+	return delay
+}
