e12edfd9e266a60ab47d7cb04e231d7729271360
Cliff Brake <cbrake@bec-systems.com>
2020-08-27 14:01:51 -0400

    add utility to dump bolt dbs

---
+++ cmd/boltutil/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	bolt "go.etcd.io/bbolt"
+)
+
+func dumpBboltKeys(filepath string) {
+	db, err := bolt.Open(filepath, 0666, nil)
+	if err != nil {
+		log.Fatal("Error opening db: ", err)
+	}
+
+	defer db.Close()
+
+	db.View(func(tx *bolt.Tx) error {
+		tx.ForEach(func(name []byte, b *bolt.Bucket) error {
+			fmt.Println("Bucket: ", string(name))
+			b.ForEach(func(k, v []byte) error {
+				fmt.Printf("   key2=%s, value2=%s\n", k, v)
+				return nil
+			})
+			return nil
+		})
+		return nil
+	})
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Println("Usage boltutil <filename>")
+		os.Exit(-1)
+	}
+	dumpBboltKeys(os.Args[1])
+}
