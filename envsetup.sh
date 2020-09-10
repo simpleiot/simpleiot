21b02cff5747f5bf81f103561ab74d0a7a0070a5
Cliff Brake <cbrake@bec-systems.com>
2020-09-10 14:00:20 -0400

    more release improvements

--- envsetup.sh
+++ envsetup.sh
@@ -195,6 +195,6 @@ siot_goreleaser_build() {
 
 siot_goreleaser_release() {
   #TODO add depend build to goreleaser config
-  siot_build_dependencies
+  siot_build_dependencies --optimize
   goreleaser release
 }
