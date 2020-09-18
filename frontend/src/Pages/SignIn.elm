de4e94bb164766d01b880a0eb489eabb5e9f8093
Cliff Brake <cbrake@bec-systems.com>
2020-09-18 14:43:18 -0400

    signin page is working now

--- frontend/src/Pages/SignIn.elm
+++ frontend/src/Pages/SignIn.elm
@@ -1,12 +1,19 @@
 module Pages.SignIn exposing (Model, Msg, Params, page)
 
-import Api.Auth as Auth
+import Api.Auth exposing (Auth)
 import Api.Data exposing (Data)
 import Browser.Navigation exposing (Key)
+import Element exposing (..)
+import Element.Font as Font
+import Element.Input as Input
 import Shared
 import Spa.Document exposing (Document)
+import Spa.Generated.Route as Route
 import Spa.Page as Page exposing (Page)
 import Spa.Url as Url exposing (Url)
+import UI.Form as Form
+import UI.Style as Style
+import Utils.Route
 
 
 page : Page Params Model Msg
@@ -30,7 +37,7 @@ type alias Params =
 
 
 type alias Model =
-    { authCred : Data Auth.Cred
+    { auth : Data Auth
     , key : Key
     , email : String
     , password : String
@@ -40,9 +47,9 @@ type alias Model =
 init : Shared.Model -> Url Params -> ( Model, Cmd Msg )
 init shared { key } =
     ( Model
-        (case shared.authCred of
-            Just cred ->
-                Api.Data.Success cred
+        (case shared.auth of
+            Just auth ->
+                Api.Data.Success auth
 
             Nothing ->
                 Api.Data.NotAsked
@@ -59,19 +66,58 @@ init shared { key } =
 
 
 type Msg
-    = ReplaceMe
+    = EditEmail String
+    | EditPass String
+    | SignIn
+    | GotUser (Data Auth)
+    | NoOp
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        ReplaceMe ->
+        EditEmail email ->
+            ( { model | email = email }, Cmd.none )
+
+        EditPass password ->
+            ( { model | password = password }, Cmd.none )
+
+        SignIn ->
+            ( model
+            , Api.Auth.login
+                { user =
+                    { email = model.email
+                    , password = model.password
+                    }
+                , onResponse = GotUser
+                }
+            )
+
+        NoOp ->
             ( model, Cmd.none )
+
+        GotUser auth ->
+            ( { model | auth = auth }
+            , case Api.Data.toMaybe auth of
+                Just auth_ ->
+                    Utils.Route.navigate model.key Route.Top
+
+                Nothing ->
+                    Cmd.none
+            )
 
 
 save : Model -> Shared.Model -> Shared.Model
-save _ shared =
-    shared
+save model shared =
+    { shared
+        | auth =
+            case Api.Data.toMaybe model.auth of
+                Just auth ->
+                    Just auth
+
+                Nothing ->
+                    shared.auth
+    }
 
 
 load : Shared.Model -> Model -> ( Model, Cmd Msg )
@@ -80,7 +126,7 @@ load _ model =
 
 
 subscriptions : Model -> Sub Msg
-subscriptions model =
+subscriptions _ =
     Sub.none
 
 
@@ -90,6 +136,36 @@ subscriptions model =
 
 view : Model -> Document Msg
 view model =
-    { title = "SignIn"
-    , body = []
+    { title = "SIOT SignIn"
+    , body =
+        [ el [ centerX, centerY ] <|
+            column
+                [ spacing 32 ]
+                [ el [ Font.size 24, Font.semiBold ]
+                    (text "Sign in")
+                , column [ spacing 16 ]
+                    [ Input.email
+                        []
+                        { onChange = \e -> EditEmail e
+                        , text = model.email
+                        , placeholder = Just <| Input.placeholder [] <| text "email"
+                        , label = Input.labelAbove [] <| text "Email"
+                        }
+                    , Input.newPassword
+                        []
+                        { onChange = \p -> EditPass p
+                        , show = False
+                        , text = model.password
+                        , placeholder = Just <| Input.placeholder [] <| text "password"
+                        , label = Input.labelAbove [] <| text "Password"
+                        }
+                    , el [ alignRight ] <|
+                        if String.isEmpty model.email then
+                            Form.button "Sign In" Style.colors.gray NoOp
+
+                        else
+                            Form.button "Sign In" Style.colors.blue SignIn
+                    ]
+                ]
+        ]
     }
