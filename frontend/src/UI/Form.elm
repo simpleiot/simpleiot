266e41bcc0b540b53fa3520d7937d3135107804c
Cliff Brake <cbrake@bec-systems.com>
2020-12-02 12:30:41 -0500

    add function for option input

--- frontend/src/UI/Form.elm
+++ frontend/src/UI/Form.elm
@@ -2,6 +2,8 @@ module UI.Form exposing
     ( button
     , buttonRow
     , label
+    , nodeNumberInput
+    , nodeOptionInput
     , nodeTextInput
     , viewTextProperty
     )
@@ -94,4 +96,74 @@ nodeTextInput o pointName lbl =
         , text = Point.getPointText o.node.points pointName
         , placeholder = Nothing
         , label = Input.labelLeft [ width (px 100) ] <| el [ alignRight ] <| text <| lbl ++ ":"
+        }
+
+
+nodeNumberInput :
+    { onEditNodePoint : String -> Point -> msg
+    , node : Node
+    , now : Time.Posix
+    }
+    -> String
+    -> String
+    -> Element msg
+nodeNumberInput o pointName lbl =
+    let
+        currentValue =
+            Point.getPointValue o.node.points pointName
+    in
+    Input.text
+        []
+        { onChange =
+            \d ->
+                let
+                    v =
+                        if d == "" then
+                            0
+
+                        else
+                            Maybe.withDefault currentValue <| String.toFloat d
+                in
+                o.onEditNodePoint o.node.id
+                    (Point "" pointName 0 o.now v "" 0 0)
+        , text =
+            if currentValue == 0 then
+                ""
+
+            else
+                String.fromFloat currentValue
+        , placeholder = Nothing
+        , label = Input.labelLeft [ width (px 100) ] <| el [ alignRight ] <| text <| lbl ++ ":"
+        }
+
+
+nodeOptionInput :
+    { onEditNodePoint : String -> Point -> msg
+    , node : Node
+    , now : Time.Posix
+    }
+    -> String
+    -> String
+    -> List ( String, String )
+    -> Element msg
+nodeOptionInput o pointName lbl options =
+    Input.radio
+        [ spacing 6 ]
+        { onChange =
+            \sel ->
+                o.onEditNodePoint o.node.id
+                    (Point "" pointName 0 o.now 0 sel 0 0)
+        , label =
+            Input.labelLeft [ padding 12, width (px 100) ] <|
+                el [ alignRight ] <|
+                    text <|
+                        lbl
+                            ++ ":"
+        , selected = Just <| Point.getPointText o.node.points pointName
+        , options =
+            List.map
+                (\opt ->
+                    Input.option (Tuple.first opt) (text (Tuple.second opt))
+                )
+                options
         }
