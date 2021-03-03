3ea24cae2a292590fb88b58d1646d2c61e7f2969
Cliff Brake <cbrake@bec-systems.com>
2021-03-03 15:26:24 -0500

    add ports API for writing to clipboard

--- frontend/public/ports.js
+++ frontend/public/ports.js
@@ -1,17 +1,31 @@
 // On load, listen to Elm!
-window.addEventListener('load', _ => {
+window.addEventListener("load", (_) => {
   window.ports = {
     init: (app) =>
-      app.ports.outgoing.subscribe(({ action, data }) =>
+      app.ports.out.subscribe(({ action, data }) =>
         actions[action]
           ? actions[action](data)
           : console.warn(`I didn't recognize action "${action}".`)
-      )
-  }
-})
+      ),
+  };
+});
 
 // maps actions to functions!
 const actions = {
-  'LOG': (message) =>
-    console.log(`From Elm:`, message)
-}
+  LOG: (message) => console.log(`From Elm:`, message),
+  CLIPBOARD: (message) => writeClipboard(message),
+};
+
+console.log("Simple IoT Javascript code");
+
+var writeClipboard = (data) => {
+  navigator.clipboard
+    .writeText(data)
+    .then(() => {
+      console.log("copy success");
+      // Success!
+    })
+    .catch((err) => {
+      console.log("Something went wrong", err);
+    });
+};
