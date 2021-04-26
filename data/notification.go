f1f04e051cc3a38f93793a0b92662249890b302f
Cliff Brake <cbrake@bec-systems.com>
2021-04-26 13:43:38 -0400

    add parent field to notification

--- data/notification.go
+++ data/notification.go
@@ -8,6 +8,7 @@ import (
 // Notification represents a message sent by a node
 type Notification struct {
 	ID         string
+	Parent     string
 	SourceNode string
 	Subject    string
 	Message    string
@@ -17,6 +18,7 @@ type Notification struct {
 func (n *Notification) ToPb() ([]byte, error) {
 	pbNot := pb.Notification{
 		Id:         n.ID,
+		Parent:     n.Parent,
 		SourceNode: n.SourceNode,
 		Subject:    n.Subject,
 		Msg:        n.Message,
@@ -36,6 +38,7 @@ func PbDecodeNotification(data []byte) (Notification, error) {
 
 	return Notification{
 		ID:         pbNot.Id,
+		Parent:     pbNot.Parent,
 		SourceNode: pbNot.SourceNode,
 		Subject:    pbNot.Subject,
 		Message:    pbNot.Msg,
