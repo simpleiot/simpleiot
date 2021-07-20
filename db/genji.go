037dc8263df8cd388ad564a87c2de37ba932c4d5
Cliff Brake <cbrake@bec-systems.com>
2021-07-20 10:57:51 -0400

    Revert "fix issue with sending points to create nodes"
    This reverts commit 79a18594ea278976e81086792c818416a5cf2de8.

--- db/genji.go
+++ db/genji.go
@@ -417,9 +417,7 @@ func (gen *Db) edgePoints(nodeID, parentID string, points data.Points) error {
 
 		ne, err := nec.getNodeAndEdges(edge.Down)
 		if err != nil {
-			if err != genjierrors.ErrDocumentNotFound {
-				return fmt.Errorf("getNodeAndEdges error: %w", err)
-			}
+			return fmt.Errorf("getNodeAndEdges error: %w", err)
 		}
 
 		if newEdge {
