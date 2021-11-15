ac1916c31496ff80ddef4b2ff40d7a39621b1b5c
Cliff Brake <cbrake@bec-systems.com>
2021-11-15 12:16:33 -0500

    Revert "WIP refactor node/edge cache"
    This reverts commit 7afafda26cdcb6f38e7aeecdd6c13293d08191e1.

--- store/hash.go
+++ store/hash.go
@@ -9,7 +9,6 @@ import (
 )
 
 // updateHash updates the hash in all the upstream edges
-// this is just a bad idea to be reaching back into data structures ...
 func updateHash(node *data.Node, upEdges []*data.Edge, downEdges []*data.Edge) {
 	// downstream edge hashes are used in the hash calculation, so sort them first
 	sort.Sort(data.ByHash(downEdges))
@@ -35,30 +34,4 @@ func updateHash(node *data.Node, upEdges []*data.Edge, downEdges []*data.Edge) {
 
 		up.Hash = h.Sum(nil)
 	}
-}
-
-// calcHash calculates the hash for a node. downEdges should be sorted by
-// hash before calling this function
-func calcHash(node data.Node, upEdge data.Edge, downEdges []data.Edge) []byte {
-	h := md5.New()
-
-	// downstream edge hashes are used in the hash calculation, so sort them first
-
-	for _, p := range upEdge.Points {
-		d := make([]byte, 8)
-		binary.LittleEndian.PutUint64(d, uint64(p.Time.UnixNano()))
-		h.Write(d)
-	}
-
-	for _, p := range node.Points {
-		d := make([]byte, 8)
-		binary.LittleEndian.PutUint64(d, uint64(p.Time.UnixNano()))
-		h.Write(d)
-	}
-
-	for _, downEdge := range downEdges {
-		h.Write(downEdge.Hash)
-	}
-
-	return h.Sum(nil)
 }
