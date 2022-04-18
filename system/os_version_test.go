c17e98fedf3e3ad89cbc0b564a23d13336a9e97d
Cliff Brake <cbrake@bec-systems.com>
2022-04-18 11:44:58 -0400

    fix tests

--- system/os_version_test.go
+++ system/os_version_test.go
@@ -8,15 +8,22 @@ import (
 )
 
 func TestReadOSRelease(t *testing.T) {
-	rel, err := ReadOSVersion()
+	/* will only work on some Linux systems
+	rel, err := ReadOSVersion("VERSION")
 	// If there is no error, we have a problem
 	if err != nil {
 		t.Error("Error reading version: ", err)
 	}
 
 	t.Log("/etc/os-release contains a valid version?", rel)
+	*/
 
-	v, err := parseVersion([]byte("VERSION_ID=\"1.2\"\nTesting with quotes"))
+	v, err := parseVersion([]byte("VERSION_ID=\"1.2\"\nTesting with quotes"), "VERSION_ID")
+
+	if err != nil {
+		t.Error("Got error parsing version: ", err)
+	}
+
 	exp := semver.Version{
 		Major: 1,
 		Minor: 2,
@@ -28,11 +35,15 @@ func TestReadOSRelease(t *testing.T) {
 		t.Error("Did not get expected version")
 	}
 
-	v, err = parseVersion([]byte("VERSION_ID=1.2.352\nTesting without quotes"))
+	v, err = parseVersion([]byte("VERSION_ID=1.2.352\nTesting without quotes"), "VERSION_ID")
 	exp = semver.Version{
 		Major: 1,
 		Minor: 2,
 		Patch: 352,
+	}
+
+	if err != nil {
+		t.Error("Got error parsing version: ", err)
 	}
 
 	if v.NE(exp) {
