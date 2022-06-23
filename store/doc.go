c077c65f84ad920eca94122f8c27773beb670f8e
Cliff Brake <cbrake@bec-systems.com>
2022-06-23 11:09:58 -0400

    update doc

--- store/doc.go
+++ store/doc.go
@@ -1,4 +1,5 @@
-// Package store implements database store code -- currently Genji and Influxdb. Also contains NATS
-// handlers to receive data. This allows us to keep the db write functions private and force
-// all write data through NATS, and thus makes it easy to observe any data changes.
+// Package store implements the SIOT data store and processes messages.
+// Currently data is stored in Genji and Influxdb.
+// Direct DB access is not provided and all write data goes through NATS,
+// thus making it easy to observe any data changes.
 package store
