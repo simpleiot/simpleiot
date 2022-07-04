c40aa083688761334a8cc58053029b5909173b32
Cliff Brake <cbrake@bec-systems.com>
2022-07-04 16:28:23 -0400

    test that fires up server now works

--- client/manager_test.go
+++ client/manager_test.go
@@ -1,11 +1,16 @@
 package client_test
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"testing"
+	"time"
 
 	"github.com/simpleiot/simpleiot/client"
 	"github.com/simpleiot/simpleiot/data"
+	"github.com/simpleiot/simpleiot/server"
+	"github.com/simpleiot/simpleiot/store"
 )
 
 type testNode struct {
@@ -32,26 +37,49 @@ func (tnc *testNodeClient) Run(c <-chan data.Point) {
 func (tnc *testNodeClient) Stop() {
 }
 
+var testServerOptions = server.Options{
+	StoreType:    store.StoreTypeMemory,
+	NatsPort:     4990,
+	HTTPPort:     "8990",
+	NatsHTTPPort: 8991,
+	NatsWSPort:   8992,
+	NatsServer:   "nats://localhost:4990",
+}
+
 func TestManager(t *testing.T) {
-	/*
-		s, nc, err := test.StartServer()
-		defer s.Stop()
+	s, nc, err := server.NewServer(testServerOptions)
 
-		if err != nil {
-			t.Fatal("Test server failed to start: ", err)
-		}
+	if err != nil {
+		t.Fatal("Error starting siot server: ", err)
+	}
 
-		nodes, err := client.GetNode(nc, "root", "")
+	stopped := make(chan struct{})
 
-		if err != nil {
-			t.Fatal("Error getting root node: ", err)
-		}
+	go func() {
+		err := s.Start()
+		log.Println("Server start returned: ", err)
+		close(stopped)
+	}()
 
-		log.Println("CLIFF: rootnodes: ", nodes)
+	defer func() {
+		s.Stop(nil)
+		<-stopped
+	}()
 
-		m := client.NewManager("rootid", newTestNodeClient)
-		_ = m
-	*/
+	ctx, _ := context.WithTimeout(context.Background(), time.Second*5)
+	err = s.WaitStart(ctx)
+
+	if err != nil {
+		t.Fatal("Error waiting for server to start: ", err)
+	}
+
+	nodes, err := client.GetNode(nc, "root", "")
+
+	if err != nil {
+		t.Fatal("Error getting root node: ", err)
+	}
+
+	log.Println("CLIFF: rootnodes: ", nodes)
 }
 
 func TestManager2(t *testing.T) {
