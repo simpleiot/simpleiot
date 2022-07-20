43b8e915d48ddf2ddd2f76dc919f22a0a643c4cf
Cliff Brake <cbrake@bec-systems.com>
2022-07-20 17:32:38 -0400

    work on cobs wrapper

---
+++ client/cobs-wrapper.go
@@ -0,0 +1,35 @@
+package client
+
+import (
+	"io"
+
+	"github.com/dim13/cobs"
+)
+
+type cobsWrapper struct {
+	dev io.ReadWriteCloser
+}
+
+func newCobsWrapper(dev io.ReadWriteCloser) *cobsWrapper {
+	return &cobsWrapper{dev: dev}
+}
+
+func (cw *cobsWrapper) Read(b []byte) (int, error) {
+	// FIXME the +50 below is probably not great
+	buf := make([]byte, len(b)+50)
+	c, err := cw.dev.Read(buf)
+	if err != nil {
+		return 0, err
+	}
+	buf = buf[0:c]
+	dec := cobs.Decode(buf)
+	return copy(b, dec), nil
+}
+
+func (cw *cobsWrapper) Write(b []byte) (int, error) {
+	return 0, nil
+}
+
+func (cw *cobsWrapper) Close() error {
+	return cw.dev.Close()
+}
---
+++ client/cobs-wrapper_test.go
@@ -0,0 +1,67 @@
+package client
+
+import (
+	"bytes"
+	"reflect"
+	"sync"
+	"testing"
+
+	"github.com/dim13/cobs"
+)
+
+type ioSim struct {
+	out *bytes.Buffer
+	in  *bytes.Buffer
+	m   *sync.Mutex
+}
+
+// newIoSim creates a new IO sim and returns the A and B side of an IO simulator
+// that implements a ReadWriteCloser
+func newIoSim() (*ioSim, *ioSim) {
+	var a2b bytes.Buffer
+	var b2a bytes.Buffer
+	var m sync.Mutex
+
+	a := ioSim{&a2b, &b2a, &m}
+	b := ioSim{&b2a, &a2b, &m}
+
+	return &a, &b
+}
+
+func (ios *ioSim) Write(d []byte) (int, error) {
+	ios.m.Lock()
+	defer ios.m.Unlock()
+	return ios.in.Write(d)
+}
+
+func (ios *ioSim) Read(d []byte) (int, error) {
+	ios.m.Lock()
+	defer ios.m.Unlock()
+	return ios.out.Read(d)
+}
+
+func (ios *ioSim) Close() error {
+	return nil
+}
+
+func TestCobsWrapper(t *testing.T) {
+	d := []byte{1, 2, 3, 0, 4, 5, 6}
+
+	a, b := newIoSim()
+
+	cw := newCobsWrapper(b)
+
+	a.Write(cobs.Encode(d))
+
+	buf := make([]byte, 500)
+
+	c, err := cw.Read(buf)
+	if err != nil {
+		t.Fatal("Error reading cw: ", err)
+	}
+	buf = buf[0:c]
+
+	if !reflect.DeepEqual(buf, d) {
+		t.Fatal("Read data does not match")
+	}
+}
