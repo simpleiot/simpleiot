1427bb7d0ded663330ec90e49c7d5a2d76441733
Cliff Brake <cbrake@bec-systems.com>
2022-07-21 20:47:47 -0400

    basic serial packet wrapper complete

---
+++ client/serial-wrapper_test.go
@@ -0,0 +1,48 @@
+package client
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/simpleiot/simpleiot/data"
+)
+
+func TestSerialEncodeDecode(t *testing.T) {
+	seq := byte(123)
+	subject := "test/subject/23"
+	points := data.Points{
+		{Type: data.PointTypeDescription, Text: "node description"},
+		{Type: data.PointTypeValue, Value: 23.53},
+	}
+
+	d, err := SerialEncode(seq, subject, points)
+	if err != nil {
+		t.Fatal("Error encoding data: ", err)
+	}
+
+	seqD, subjectD, pointsD, err := SerialDecode(d)
+
+	if err != nil {
+		t.Error("Decode error: ", err)
+	}
+
+	if seq != seqD {
+		t.Error("sequence mismatch")
+	}
+
+	if subject != subjectD {
+		t.Error("subject mismatch")
+	}
+
+	if len(points) != len(pointsD) {
+		t.Error("points len mismatch")
+		fmt.Printf("points: %+v\n", points)
+		fmt.Printf("pointsD: %+v\n", pointsD)
+	}
+
+	if points[0].Type != pointsD[0].Type {
+		t.Error("points[0] description mismatch")
+		fmt.Printf("points: %+v\n", points[0])
+		fmt.Printf("pointsD: %+v\n", pointsD[0])
+	}
+}
