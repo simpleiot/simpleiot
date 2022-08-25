ba3d7b58d7a9301a07ac0bca646542104e308c7b
Cliff Brake <cbrake@bec-systems.com>
2022-08-25 17:33:30 -0400

    add serial encode example

---
+++ cmd/serial-encode/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/simpleiot/simpleiot/client"
+	"github.com/simpleiot/simpleiot/data"
+	"github.com/simpleiot/simpleiot/test"
+)
+
+func main() {
+	seq := byte(23)
+
+	subject := ""
+
+	points := data.Points{
+		{Type: "temp", Value: 23},
+		{Type: "description", Text: "temp sensor"},
+	}
+
+	data, err := client.SerialEncode(seq, subject, points)
+
+	if err != nil {
+		log.Fatal("Encode error: ", err)
+	}
+
+	fmt.Println("Encoded data: ", test.HexDump(data))
+
+}
