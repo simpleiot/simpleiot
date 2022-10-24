c69f932e566f59168fe163440605673e26dcd0e8
Cliff Brake <cbrake@bec-systems.com>
2022-10-24 17:22:36 -0400

    test large messages

--- client/serial_test.go
+++ client/serial_test.go
@@ -186,3 +186,98 @@ func TestSerial(t *testing.T) {
 		t.Error("Error in pump setting received by MCU")
 	}
 }
+
+func TestSerialLargeMessage(t *testing.T) {
+	// Start up a SIOT test server for this test
+	nc, root, stop, err := server.TestServer()
+	_ = nc
+
+	if err != nil {
+		t.Fatal("Error starting test server: ", err)
+	}
+
+	defer stop()
+
+	// the test.Fifo is used to emulate a serial port
+	// channel during this test. The A side is used by the
+	// this test, and the B side is used by the serial
+	// client.
+	fifo, err := test.NewFifoA("serialfifo")
+	if err != nil {
+		t.Fatal("Error starting fifo: ", err)
+	}
+
+	fifoW := client.NewCobsWrapper(fifo)
+	defer fifoW.Close()
+
+	serialTest := client.SerialDev{
+		ID:          "ID-serial",
+		Parent:      root.ID,
+		Description: "test serial",
+		// when Port is set to the magic value of "serialfifo", the serial
+		// client opens a unix fifo instead of a real serial port. This allows
+		// us to send/receive data to/from serial client during
+		// testing without needing real serial hardware.
+		Port: "serialfifo",
+	}
+
+	// hydrate database with test data
+	err = client.SendNodeType(nc, serialTest, "test")
+	if err != nil {
+		t.Fatal("Error sending node: ", err)
+	}
+
+	// set up watcher for node
+	getNode, stopWatcher, err := client.NodeWatcher[client.SerialDev](nc, serialTest.ID, serialTest.Parent)
+	if err != nil {
+		t.Fatal("Error setting up node watcher")
+	}
+
+	defer stopWatcher()
+
+	start := time.Now()
+
+	// wait for node to be populated
+	for {
+		cur := getNode()
+		if cur.ID == serialTest.ID {
+			break
+		}
+		if time.Since(start) > time.Second {
+			t.Fatal("Timeout waiting for serial node")
+		}
+		<-time.After(time.Millisecond * 10)
+	}
+
+	var points data.Points
+
+	for i := 0; i < 10; i++ {
+		points = append(points, data.Point{Type: "testPoint", Index: float64(i), Value: float64(i * 2)})
+	}
+
+	packet, err := client.SerialEncode(1, "", points)
+	if err != nil {
+		t.Fatal("Error encoding serial packet: ", err)
+	}
+
+	fmt.Println("len(packet): ", len(packet))
+	fmt.Println("Rx: ", getNode().Rx)
+
+	_, err = fifoW.Write(packet)
+	if err != nil {
+		t.Fatal("Error writing pb data to fifo: ", err)
+	}
+
+	// wait for point to show up in node
+	start = time.Now()
+	for {
+		cur := getNode()
+		if cur.Rx >= 1 {
+			break
+		}
+		if time.Since(start) > time.Second {
+			t.Fatal("Timeout waiting for packet to get set")
+		}
+		<-time.After(time.Millisecond * 100)
+	}
+}
