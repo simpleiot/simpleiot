a3fe7ece8cf68fe11d8566d3e5de0f4df773517b
Cliff Brake <cbrake@bec-systems.com>
2022-12-28 11:39:43 -0500

    get frontend clipboard working again

--- frontend/public/main.js
+++ frontend/public/main.js
@@ -1,5 +1,6 @@
 // import { BLE } from "./ble.js"
 
+// eslint-disable-next-line no-undef
 const app = Elm.Main.init({
 	flags: JSON.parse(localStorage.getItem("storage")),
 })
@@ -8,6 +9,39 @@ app.ports.save_.subscribe((storage) => {
 	localStorage.setItem("storage", JSON.stringify(storage))
 	app.ports.load_.send(storage)
 })
+
+app.ports.out.subscribe(({ action, data }) =>
+	actions[action]
+		? actions[action](data)
+		: console.warn(`I didn't recognize action "${action}".`)
+)
+
+// maps actions to functions!
+const actions = {
+	LOG: (message) => console.log(`From Elm:`, message),
+	CLIPBOARD: (message) => {
+		console.log("CLIFF: CLIPBOARD")
+		if (navigator.clipboard) {
+			writeClipboard(message)
+		} else {
+			console.log("clipboard not available")
+		}
+	},
+}
+
+console.log("Simple IoT Javascript code")
+
+const writeClipboard = (data) => {
+	navigator.clipboard
+		.writeText(data)
+		.then(() => {
+			// FIXME, should probably send something back to elm
+			// Success!
+		})
+		.catch((err) => {
+			console.log("Something went wrong", err)
+		})
+}
 
 /*
 export const main = (app) => {
