8cd8e80ad8f849c17592a47d1e3136b17544bf2d
Cliff Brake <cbrake@bec-systems.com>
2023-06-09 12:27:12 -0400

    work on abstracting shelly component types a bit better

---
+++ client/shelly-io-client.go
@@ -0,0 +1,232 @@
+package client
+
+import (
+	"log"
+	"time"
+
+	"github.com/nats-io/nats.go"
+	"github.com/simpleiot/simpleiot/data"
+	"golang.org/x/exp/constraints"
+)
+
+// ShellyIOClient is a SIOT particle client
+type ShellyIOClient struct {
+	nc              *nats.Conn
+	config          ShellyIo
+	points          data.Points
+	stop            chan struct{}
+	newPoints       chan NewPoints
+	newEdgePoints   chan NewPoints
+	newShellyPoints chan NewPoints
+	errorCount      int
+	comps           []shellyComp
+}
+
+// NewShellyIOClient ...
+func NewShellyIOClient(nc *nats.Conn, config ShellyIo) Client {
+	// we need a copy of points with timestamps so we know when to send up new data
+	ne, err := data.Encode(config)
+	if err != nil {
+		log.Println("Error encoding shelly config: ", err)
+	}
+
+	return &ShellyIOClient{
+		nc:              nc,
+		config:          config,
+		comps:           shellyCompMap[config.Type],
+		points:          ne.Points,
+		stop:            make(chan struct{}),
+		newPoints:       make(chan NewPoints),
+		newEdgePoints:   make(chan NewPoints),
+		newShellyPoints: make(chan NewPoints),
+	}
+}
+
+// Run runs the main logic for this client and blocks until stopped
+func (sioc *ShellyIOClient) Run() error {
+	log.Println("Starting shelly IO client: ", sioc.config.Description)
+
+	sampleRate := time.Second * 2
+	sampleRateOffline := time.Minute * 10
+
+	syncConfigTicker := time.NewTicker(sampleRateOffline)
+	sampleTicker := time.NewTicker(sampleRate)
+
+	if sioc.config.Offline {
+		sampleTicker = time.NewTicker(sampleRateOffline)
+	}
+
+	shellyError := func() {
+		sioc.errorCount++
+		if !sioc.config.Offline && sioc.errorCount > 5 {
+			log.Printf("Shelly device %v is offline", sioc.config.Description)
+			sioc.config.Offline = true
+			err := SendNodePoint(sioc.nc, sioc.config.ID, data.Point{
+				Type: data.PointTypeOffline, Value: 1}, false)
+
+			if err != nil {
+				log.Println("ShellyIO: error sending node point: ", err)
+			}
+			sampleTicker = time.NewTicker(sampleRateOffline)
+		}
+	}
+
+	shellyCommOK := func() {
+		sioc.errorCount = 0
+		if sioc.config.Offline {
+			log.Printf("Shelly device %v is online", sioc.config.Description)
+			sioc.config.Offline = false
+			err := SendNodePoint(sioc.nc, sioc.config.ID, data.Point{
+				Type: data.PointTypeOffline, Value: 0}, false)
+
+			if err != nil {
+				log.Println("ShellyIO: error sending node point: ", err)
+			}
+			sampleTicker = time.NewTicker(sampleRate)
+		}
+	}
+
+	syncConfig := func() {
+		config, err := sioc.config.getConfig()
+		if err != nil {
+			shellyError()
+			log.Println("Error getting shelly IO settings: ", sioc.config.Desc(), err)
+			return
+		}
+
+		shellyCommOK()
+
+		if sioc.config.Description == "" && config.Name != "" {
+			sioc.config.Description = config.Name
+			err := SendNodePoint(sioc.nc, sioc.config.ID, data.Point{
+				Type: data.PointTypeDescription, Text: config.Name}, false)
+			if err != nil {
+				log.Println("Error sending shelly io description: ", err)
+			}
+		} else if sioc.config.Description != config.Name {
+			err := sioc.config.SetName(sioc.config.Description)
+			if err != nil {
+				log.Println("Error setting name on Shelly device: ", err)
+			}
+		}
+	}
+
+	syncConfig()
+
+done:
+	for {
+		select {
+		case <-sioc.stop:
+			log.Println("Stopping shelly IO client: ", sioc.config.Description)
+			break done
+		case pts := <-sioc.newPoints:
+			err := data.MergePoints(pts.ID, pts.Points, &sioc.config)
+			if err != nil {
+				log.Println("error merging new points: ", err)
+			}
+
+			for _, p := range pts.Points {
+				switch p.Type {
+				case data.PointTypeDescription:
+					syncConfig()
+				case data.PointTypeDisable:
+					if p.Value == 0 {
+						sampleTicker = time.NewTicker(sampleRate)
+					} else {
+						sampleTicker.Stop()
+					}
+				case data.PointTypeOffline:
+					if p.Value == 0 {
+						// defice is online
+						// the discovery mechanism may have set the IO back online
+						sampleTicker = time.NewTicker(sampleRate)
+					} else {
+						sampleTicker = time.NewTicker(sampleRateOffline)
+					}
+				}
+			}
+
+		case pts := <-sioc.newEdgePoints:
+			err := data.MergeEdgePoints(pts.ID, pts.Parent, pts.Points, &sioc.config)
+			if err != nil {
+				log.Println("error merging new points: ", err)
+			}
+
+		case <-syncConfigTicker.C:
+			syncConfig()
+
+		case <-sampleTicker.C:
+			points, err := sioc.config.GetStatus()
+			if err != nil {
+				log.Printf("Error getting status for %v: %v\n", sioc.config.Description, err)
+				shellyError()
+				break
+			}
+
+			if sioc.config.IsSettableOnOff() {
+				min := min(len(sioc.config.Value), len(sioc.config.ValueSet))
+				for i := 0; i < min; i++ {
+					if sioc.config.Value[i] != sioc.config.ValueSet[i] && sioc.config.Control {
+						pts, err := sioc.config.SetOnOff(i, data.FloatToBool(sioc.config.ValueSet[i]))
+						if err != nil {
+							log.Printf("Error setting %v: %v\n", sioc.config.Description, err)
+						}
+
+						if len(pts) > 0 {
+							points = append(points, pts...)
+						} else {
+							// get current status as the set did not return status
+							points, err = sioc.config.GetStatus()
+							if err != nil {
+								log.Printf("Error getting status for %v: %v\n", sioc.config.Description, err)
+								shellyError()
+								break
+							}
+						}
+					}
+				}
+			}
+
+			shellyCommOK()
+
+			newPoints := sioc.points.Merge(points, time.Minute*15)
+			if len(newPoints) > 0 {
+				err := data.MergePoints(sioc.config.ID, newPoints, &sioc.config)
+				if err != nil {
+					log.Println("shelly io: error merging newPoints: ", err)
+				}
+				err = SendNodePoints(sioc.nc, sioc.config.ID, newPoints, false)
+				if err != nil {
+					log.Println("shelly io: error sending newPoints: ", err)
+				}
+			}
+		}
+	}
+
+	// clean up
+	return nil
+}
+
+// Stop sends a signal to the Run function to exit
+func (sioc *ShellyIOClient) Stop(_ error) {
+	close(sioc.stop)
+}
+
+// Points is called by the Manager when new points for this
+// node are received.
+func (sioc *ShellyIOClient) Points(nodeID string, points []data.Point) {
+	sioc.newPoints <- NewPoints{nodeID, "", points}
+}
+
+// EdgePoints is called by the Manager when new edge points for this
+// node are received.
+func (sioc *ShellyIOClient) EdgePoints(nodeID, parentID string, points []data.Point) {
+	sioc.newEdgePoints <- NewPoints{nodeID, parentID, points}
+}
+
+func min[T constraints.Ordered](a, b T) T {
+	if a < b {
+		return a
+	}
+	return b
+}
--- client/shelly-io.go
+++ client/shelly-io.go
@@ -3,19 +3,16 @@ package client
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 	"strings"
 	"time"
 
-	"github.com/nats-io/nats.go"
 	"github.com/simpleiot/simpleiot/data"
-	"golang.org/x/exp/constraints"
 )
 
-// ShellyIOConfig describes the configuration of a Shelly device
-type ShellyIOConfig struct {
+// shellyIOConfig describes the configuration of a Shelly device
+type shellyIOConfig struct {
 	Name string `json:"name"`
 }
 
@@ -49,14 +46,16 @@ type shellyGen2SwitchSetResp struct {
 	WasOn bool `json:"wasOn"`
 }
 
-func (swi *shellyGen2SwitchStatus) toPoints() data.Points {
+func (swi *shellyGen2SwitchStatus) toPoints(index int) data.Points {
 	now := time.Now()
+	key := strconv.Itoa(index)
+	i := float32(index)
 	return data.Points{
-		{Time: now, Type: data.PointTypeValue, Value: data.BoolToFloat(swi.Output)},
-		{Time: now, Type: data.PointTypePower, Value: float64(swi.Apower)},
-		{Time: now, Type: data.PointTypeVoltage, Value: float64(swi.Voltage)},
-		{Time: now, Type: data.PointTypeCurrent, Value: float64(swi.Current)},
-		{Time: now, Type: data.PointTypeTemperature, Value: float64(swi.Temperature.TC)},
+		{Time: now, Type: data.PointTypeValue, Key: key, Index: i, Value: data.BoolToFloat(swi.Output)},
+		{Time: now, Type: data.PointTypePower, Key: key, Index: i, Value: float64(swi.Apower)},
+		{Time: now, Type: data.PointTypeVoltage, Key: key, Index: i, Value: float64(swi.Voltage)},
+		{Time: now, Type: data.PointTypeCurrent, Key: key, Index: i, Value: float64(swi.Current)},
+		{Time: now, Type: data.PointTypeTemperature, Key: key, Index: i, Value: float64(swi.Temperature.TC)},
 	}
 }
 
@@ -95,8 +94,8 @@ func (sls *shellyGen1LightStatus) toPoints() data.Points {
 	}
 }
 
-func (sg2c shellyGen2SysConfig) toSettings() ShellyIOConfig {
-	return ShellyIOConfig{
+func (sg2c shellyGen2SysConfig) toSettings() shellyIOConfig {
+	return shellyIOConfig{
 		Name: sg2c.Device.Name,
 	}
 }
@@ -146,6 +145,27 @@ var shellyGenMap = map[string]ShellyGen{
 	data.PointValueShellyTypePlugS:   ShellyGen2,
 	data.PointValueShellyTypeI4:      ShellyGen2,
 	data.PointValueShellyTypePlus1:   ShellyGen2,
+	data.PointValueShellyTypePlus2PM: ShellyGen2,
+}
+
+// Gen 2 metadata
+
+// shellComp is used to describe shelly "components" a device may support
+type shellyComp struct {
+	name  string
+	count int
+}
+
+var shellyCompMap = map[string][]shellyComp{
+	data.PointValueShellyTypeBulbDuo: {{"light", 1}},
+	data.PointValueShellyType1PM:     {{"switch", 1}},
+	data.PointValueShellyTypeI4:      {{"input", 4}},
+	data.PointValueShellyTypePlugUS:  {{"switch", 1}},
+	data.PointValueShellyTypePlugUK:  {{"switch", 1}},
+	data.PointValueShellyTypePlugIT:  {{"switch", 1}},
+	data.PointValueShellyTypePlugS:   {{"switch", 1}},
+	data.PointValueShellyTypePlus1:   {{"switch", 1}, {"input", 1}},
+	data.PointValueShellyTypePlus2PM: {{"switch", 2}, {"input", 2}},
 }
 
 var shellySettableOnOff = map[string]bool{
@@ -157,6 +177,7 @@ var shellySettableOnOff = map[string]bool{
 	data.PointValueShellyTypePlugIT:  true,
 	data.PointValueShellyTypePlugS:   true,
 	data.PointValueShellyTypePlus1:   true,
+	data.PointValueShellyTypePlus2PM: true,
 }
 
 // Gen returns generation of Shelly device
@@ -176,10 +197,10 @@ func (sio *ShellyIo) IsSettableOnOff() bool {
 }
 
 // GetConfig returns the configuration of Shelly Device
-func (sio *ShellyIo) GetConfig() (ShellyIOConfig, error) {
+func (sio *ShellyIo) getConfig() (shellyIOConfig, error) {
 	switch sio.Gen() {
 	case ShellyGen1:
-		var ret ShellyIOConfig
+		var ret shellyIOConfig
 		res, err := httpClient.Get("http://" + sio.IP + "/settings")
 		if err != nil {
 			return ret, err
@@ -207,7 +228,7 @@ func (sio *ShellyIo) GetConfig() (ShellyIOConfig, error) {
 		return config.toSettings(), err
 
 	default:
-		return ShellyIOConfig{}, fmt.Errorf("Unsupported device: %v", sio.Type)
+		return shellyIOConfig{}, fmt.Errorf("Unsupported device: %v", sio.Type)
 	}
 }
 
@@ -248,7 +269,8 @@ func (sio *ShellyIo) SetOnOff(index int, on bool) (data.Points, error) {
 			onValue = "true"
 		}
 
-		res, err := httpClient.Get("http://" + sio.IP + "/rpc/Switch.Set?id=0&on=" + onValue)
+		url := fmt.Sprintf("http://%v/rpc/Switch.Set?id=%v&on=%v", sio.IP, index, onValue)
+		res, err := httpClient.Get(url)
 		if err != nil {
 			return data.Points{}, err
 		}
@@ -270,56 +292,10 @@ func (sio *ShellyIo) SetOnOff(index int, on bool) (data.Points, error) {
 	}
 }
 
-// GetStatus gets the current status of the device
-func (sio *ShellyIo) GetStatus() (data.Points, error) {
-	switch sio.Type {
-	case data.PointValueShellyTypePlugUS,
-		data.PointValueShellyTypePlugUK,
-		data.PointValueShellyTypePlugIT,
-		data.PointValueShellyTypePlugS,
-		data.PointValueShellyTypePlus1:
-		res, err := httpClient.Get("http://" + sio.IP + "/rpc/Switch.GetStatus?id=0")
-		if err != nil {
-			return data.Points{}, err
-		}
-		defer res.Body.Close()
-		if res.StatusCode != http.StatusOK {
-			return data.Points{}, fmt.Errorf("Shelly GetConfig returned an error code: %v", res.StatusCode)
-		}
+func (sio *ShellyIo) gen1GetLight(count int) (data.Points, error) {
+	ret := data.Points{}
 
-		var status shellyGen2SwitchStatus
-
-		err = json.NewDecoder(res.Body).Decode(&status)
-		if err != nil {
-			return data.Points{}, err
-		}
-		return status.toPoints(), nil
-
-	case data.PointValueShellyTypeI4:
-		var points data.Points
-		for channel := 0; channel < 4; channel++ {
-			res, err := httpClient.Get("http://" + sio.IP + "/rpc/Input.GetStatus?id=" + strconv.Itoa(channel))
-			if err != nil {
-				return data.Points{}, err
-			}
-			defer res.Body.Close()
-			if res.StatusCode != http.StatusOK {
-				return data.Points{}, fmt.Errorf("Shelly GetConfig returned an error code: %v", res.StatusCode)
-			}
-
-			var status shellyGen2InputStatus
-
-			err = json.NewDecoder(res.Body).Decode(&status)
-			if err != nil {
-				return data.Points{}, err
-			}
-
-			points = append(points, status.toPoints()...)
-		}
-
-		return points, nil
-
-	case data.PointValueShellyTypeBulbDuo:
+	for i := 0; i < count; i++ {
 		res, err := httpClient.Get("http://" + sio.IP + "/light/0")
 		if err != nil {
 			return data.Points{}, err
@@ -335,10 +311,111 @@ func (sio *ShellyIo) GetStatus() (data.Points, error) {
 		if err != nil {
 			return data.Points{}, err
 		}
-		return status.toPoints(), nil
-	default:
-		return data.Points{}, nil
+
+		ret = append(ret, status.toPoints()...)
 	}
+
+	return ret, nil
+}
+
+func (sio *ShellyIo) gen2GetSwitch(count int) (data.Points, error) {
+	ret := data.Points{}
+
+	for i := 0; i < count; i++ {
+
+		res, err := httpClient.Get("http://" + sio.IP + "/rpc/Switch.GetStatus?id=0")
+		if err != nil {
+			return data.Points{}, err
+		}
+		defer res.Body.Close()
+		if res.StatusCode != http.StatusOK {
+			return data.Points{}, fmt.Errorf("Shelly GetConfig returned an error code: %v", res.StatusCode)
+		}
+
+		var status shellyGen2SwitchStatus
+
+		err = json.NewDecoder(res.Body).Decode(&status)
+		if err != nil {
+			return data.Points{}, err
+		}
+		pts := status.toPoints(i)
+		ret = append(ret, pts...)
+	}
+
+	return ret, nil
+}
+
+func (sio *ShellyIo) gen2GetInput(count int) (data.Points, error) {
+	var points data.Points
+	for channel := 0; channel < count; channel++ {
+		res, err := httpClient.Get("http://" + sio.IP + "/rpc/Input.GetStatus?id=" + strconv.Itoa(channel))
+		if err != nil {
+			return data.Points{}, err
+		}
+		defer res.Body.Close()
+		if res.StatusCode != http.StatusOK {
+			return data.Points{}, fmt.Errorf("Shelly GetConfig returned an error code: %v", res.StatusCode)
+		}
+
+		var status shellyGen2InputStatus
+
+		err = json.NewDecoder(res.Body).Decode(&status)
+		if err != nil {
+			return data.Points{}, err
+		}
+
+		points = append(points, status.toPoints()...)
+	}
+
+	return points, nil
+}
+
+// GetStatus gets the current status of the device
+func (sio *ShellyIo) GetStatus() (data.Points, error) {
+	ret := data.Points{}
+
+	// TODO: this needs clean up some to be data driven instead of all the if statements
+	gen := sio.Gen()
+
+	if cnt := sio.getCompCount("switch"); cnt > 0 {
+		if gen == ShellyGen1 {
+			_ = gen
+			// TODO: need to add gen 1 support for switch status
+		}
+		if gen == ShellyGen2 {
+			pts, err := sio.gen2GetSwitch(cnt)
+			if err != nil {
+				return nil, err
+			}
+			ret = append(ret, pts...)
+		}
+	}
+
+	if cnt := sio.getCompCount("input"); cnt > 0 {
+		if gen == ShellyGen1 {
+			_ = gen
+			// TODO: need to add gen 1 support for input status
+		}
+		if gen == ShellyGen2 {
+			pts, err := sio.gen2GetInput(cnt)
+			if err != nil {
+				return nil, err
+			}
+			ret = append(ret, pts...)
+		}
+	}
+
+	if cnt := sio.getCompCount("light"); cnt > 0 {
+		if gen == ShellyGen1 {
+			pts, err := sio.gen1GetLight(cnt)
+			if err != nil {
+				return nil, err
+			}
+			ret = append(ret, pts...)
+		}
+	}
+
+	return ret, nil
 }
 
 type shellyGen2Response struct {
@@ -387,220 +464,18 @@ func (sio *ShellyIo) SetName(name string) error {
 	return nil
 }
 
-// ShellyIOClient is a SIOT particle client
-type ShellyIOClient struct {
-	nc              *nats.Conn
-	config          ShellyIo
-	points          data.Points
-	stop            chan struct{}
-	newPoints       chan NewPoints
-	newEdgePoints   chan NewPoints
-	newShellyPoints chan NewPoints
-	errorCount      int
-}
-
-// NewShellyIOClient ...
-func NewShellyIOClient(nc *nats.Conn, config ShellyIo) Client {
-	ne, err := data.Encode(config)
-	if err != nil {
-		log.Println("Error encoding shelly config: ", err)
-	}
-	return &ShellyIOClient{
-		nc:              nc,
-		config:          config,
-		points:          ne.Points,
-		stop:            make(chan struct{}),
-		newPoints:       make(chan NewPoints),
-		newEdgePoints:   make(chan NewPoints),
-		newShellyPoints: make(chan NewPoints),
-	}
-}
-
-// Run runs the main logic for this client and blocks until stopped
-func (sioc *ShellyIOClient) Run() error {
-	log.Println("Starting shelly IO client: ", sioc.config.Description)
-
-	sampleRate := time.Second * 2
-	sampleRateOffline := time.Minute * 10
-
-	syncConfigTicker := time.NewTicker(sampleRateOffline)
-	sampleTicker := time.NewTicker(sampleRate)
-
-	if sioc.config.Offline {
-		sampleTicker = time.NewTicker(sampleRateOffline)
+// GetCompCount returns the number of components found in the device
+func (sio *ShellyIo) getCompCount(comp string) int {
+	comps, ok := shellyCompMap[sio.Type]
+	if !ok {
+		return 0
 	}
 
-	shellyError := func() {
-		sioc.errorCount++
-		if !sioc.config.Offline && sioc.errorCount > 5 {
-			log.Printf("Shelly device %v is offline", sioc.config.Description)
-			sioc.config.Offline = true
-			err := SendNodePoint(sioc.nc, sioc.config.ID, data.Point{
-				Type: data.PointTypeOffline, Value: 1}, false)
-
-			if err != nil {
-				log.Println("ShellyIO: error sending node point: ", err)
-			}
-			sampleTicker = time.NewTicker(sampleRateOffline)
+	for _, c := range comps {
+		if c.name == comp {
+			return c.count
 		}
 	}
 
-	shellyCommOK := func() {
-		sioc.errorCount = 0
-		if sioc.config.Offline {
-			log.Printf("Shelly device %v is online", sioc.config.Description)
-			sioc.config.Offline = false
-			err := SendNodePoint(sioc.nc, sioc.config.ID, data.Point{
-				Type: data.PointTypeOffline, Value: 0}, false)
-
-			if err != nil {
-				log.Println("ShellyIO: error sending node point: ", err)
-			}
-			sampleTicker = time.NewTicker(sampleRate)
-		}
-	}
-
-	syncConfig := func() {
-		config, err := sioc.config.GetConfig()
-		if err != nil {
-			shellyError()
-			log.Println("Error getting shelly IO settings: ", sioc.config.Desc(), err)
-			return
-		}
-
-		shellyCommOK()
-
-		if sioc.config.Description == "" && config.Name != "" {
-			sioc.config.Description = config.Name
-			err := SendNodePoint(sioc.nc, sioc.config.ID, data.Point{
-				Type: data.PointTypeDescription, Text: config.Name}, false)
-			if err != nil {
-				log.Println("Error sending shelly io description: ", err)
-			}
-		} else if sioc.config.Description != config.Name {
-			err := sioc.config.SetName(sioc.config.Description)
-			if err != nil {
-				log.Println("Error setting name on Shelly device: ", err)
-			}
-		}
-	}
-
-	syncConfig()
-
-done:
-	for {
-		select {
-		case <-sioc.stop:
-			log.Println("Stopping shelly IO client: ", sioc.config.Description)
-			break done
-		case pts := <-sioc.newPoints:
-			err := data.MergePoints(pts.ID, pts.Points, &sioc.config)
-			if err != nil {
-				log.Println("error merging new points: ", err)
-			}
-
-			for _, p := range pts.Points {
-				switch p.Type {
-				case data.PointTypeDescription:
-					syncConfig()
-				case data.PointTypeDisable:
-					if p.Value == 0 {
-						sampleTicker = time.NewTicker(sampleRate)
-					} else {
-						sampleTicker.Stop()
-					}
-				case data.PointTypeOffline:
-					if p.Value == 0 {
-						// defice is online
-						// the discovery mechanism may have set the IO back online
-						sampleTicker = time.NewTicker(sampleRate)
-					} else {
-						sampleTicker = time.NewTicker(sampleRateOffline)
-					}
-				}
-			}
-
-		case pts := <-sioc.newEdgePoints:
-			err := data.MergeEdgePoints(pts.ID, pts.Parent, pts.Points, &sioc.config)
-			if err != nil {
-				log.Println("error merging new points: ", err)
-			}
-
-		case <-syncConfigTicker.C:
-			syncConfig()
-
-		case <-sampleTicker.C:
-			points, err := sioc.config.GetStatus()
-			if err != nil {
-				log.Printf("Error getting status for %v: %v\n", sioc.config.Description, err)
-				shellyError()
-				break
-			}
-
-			if sioc.config.IsSettableOnOff() {
-				min := min(len(sioc.config.Value), len(sioc.config.ValueSet))
-				for i := 0; i < min; i++ {
-					if sioc.config.Value[i] != sioc.config.ValueSet[i] && sioc.config.Control {
-						pts, err := sioc.config.SetOnOff(i, data.FloatToBool(sioc.config.ValueSet[i]))
-						if err != nil {
-							log.Printf("Error setting %v: %v\n", sioc.config.Description, err)
-						}
-
-						if len(pts) > 0 {
-							points = append(points, pts...)
-						} else {
-							// get current status as the set did not return status
-							points, err = sioc.config.GetStatus()
-							if err != nil {
-								log.Printf("Error getting status for %v: %v\n", sioc.config.Description, err)
-								shellyError()
-								break
-							}
-						}
-					}
-				}
-			}
-
-			shellyCommOK()
-
-			newPoints := sioc.points.Merge(points, time.Minute*15)
-			if len(newPoints) > 0 {
-				err := data.MergePoints(sioc.config.ID, newPoints, &sioc.config)
-				if err != nil {
-					log.Println("shelly io: error merging newPoints: ", err)
-				}
-				err = SendNodePoints(sioc.nc, sioc.config.ID, newPoints, false)
-				if err != nil {
-					log.Println("shelly io: error sending newPoints: ", err)
-				}
-			}
-		}
-	}
-
-	// clean up
-	return nil
-}
-
-// Stop sends a signal to the Run function to exit
-func (sioc *ShellyIOClient) Stop(_ error) {
-	close(sioc.stop)
-}
-
-// Points is called by the Manager when new points for this
-// node are received.
-func (sioc *ShellyIOClient) Points(nodeID string, points []data.Point) {
-	sioc.newPoints <- NewPoints{nodeID, "", points}
-}
-
-// EdgePoints is called by the Manager when new edge points for this
-// node are received.
-func (sioc *ShellyIOClient) EdgePoints(nodeID, parentID string, points []data.Point) {
-	sioc.newEdgePoints <- NewPoints{nodeID, parentID, points}
-}
-
-func min[T constraints.Ordered](a, b T) T {
-	if a < b {
-		return a
-	}
-	return b
+	return 0
 }
