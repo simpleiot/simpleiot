0ad0ae824b87e4fdb9511b148fc516399cd28078
Cliff Brake <cbrake@bec-systems.com>
2023-08-21 21:52:55 -0400

    fix possible issue in rule reporting

--- client/rule.go
+++ client/rule.go
@@ -402,6 +402,13 @@ func (rc *RuleClient) processError(errS string) {
 			}
 		}
 
+		for _, a := range rc.config.ActionsInactive {
+			if a.Error != "" {
+				found = a.Error
+				break
+			}
+		}
+
 		if found != rc.config.Error {
 			p := data.Point{
 				Type: data.PointTypeError,
