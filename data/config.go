2dcd43f245db87ed364b6c502213118cc0716782
Cliff Brake <cbrake@bec-systems.com>
2023-10-06 15:37:51 -0400

    remove legacy data structure

--- data/config.go
+++ data/config.go
@@ -1,15 +1,1 @@
 package data
-
-// would like to create a type that can represent most config items and then
-// a device (or even IO) configuration can just be an array of config entities
-// Types of data in a config:
-//  - string
-//  - float
-//  - bool
-
-// Config defines parameter of config
-type Config struct {
-	NAME  string `json:"id"`
-	IO    string
-	Value float64 `json:"value"`
-}
