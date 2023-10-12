3ae9bc9b8f3440b6b162d262d03a35497d3adb89
Cliff Brake <cbrake@bec-systems.com>
2023-10-12 15:14:27 -0400

    clean up test data structure name/location

--- client/node_test.go
+++ client/node_test.go
@@ -50,90 +50,88 @@ func TestExportNodes(t *testing.T) {
 		t.Fatal("child node is not user type")
 	}
 }
-var testReplaceNodes = data.NodeEdgeChildren{
-	NodeEdge: data.NodeEdge{
-		ID:   "123",
-		Type: "testType",
-		Points: []data.Point{
-			{Type: "nodeID", Text: "", Key: "0"},
-		},
-	},
-	Children: []data.NodeEdgeChildren{
-		{NodeEdge: data.NodeEdge{
-			ID:   "",
-			Type: "testY",
-			Points: []data.Point{
-				{Type: "description", Text: "test Y1", Key: "2"},
-				{Type: "nodeID", Text: "123", Key: "0"},
-			},
-			EdgePoints: []data.Point{
-				{Type: "role", Text: "user"},
-			},
-		},
-			Children: []data.NodeEdgeChildren{
-				{NodeEdge: data.NodeEdge{
-					ID:   "123",
-					Type: "testY",
-					Points: []data.Point{
-						{Type: "description", Text: "test Y2"},
-					},
-				}, Children: nil},
-			},
-		},
-		{NodeEdge: data.NodeEdge{
-			ID:   "",
-			Type: "testY",
-			Points: []data.Point{
-				{Type: "description", Text: "test Y1", Key: "2"},
-				{Type: "nodeID", Text: "123", Key: "0"},
-			},
-			EdgePoints: []data.Point{
-				{Type: "role", Text: "user"},
-			},
-		},
-			Children: nil,
-		},
-	},
-}
-
 func TestReplaceIDs(t *testing.T) {
-	client.ReplaceIDs(&testReplaceNodes, "parent123")
+	testNodes := data.NodeEdgeChildren{
+		NodeEdge: data.NodeEdge{
+			ID:   "123",
+			Type: "testType",
+			Points: []data.Point{
+				{Type: "nodeID", Text: "", Key: "0"},
+			},
+		},
+		Children: []data.NodeEdgeChildren{
+			{NodeEdge: data.NodeEdge{
+				ID:   "",
+				Type: "testY",
+				Points: []data.Point{
+					{Type: "description", Text: "test Y1", Key: "2"},
+					{Type: "nodeID", Text: "123", Key: "0"},
+				},
+				EdgePoints: []data.Point{
+					{Type: "role", Text: "user"},
+				},
+			},
+				Children: []data.NodeEdgeChildren{
+					{NodeEdge: data.NodeEdge{
+						ID:   "123",
+						Type: "testY",
+						Points: []data.Point{
+							{Type: "description", Text: "test Y2"},
+						},
+					}, Children: nil},
+				},
+			},
+			{NodeEdge: data.NodeEdge{
+				ID:   "",
+				Type: "testY",
+				Points: []data.Point{
+					{Type: "description", Text: "test Y1", Key: "2"},
+					{Type: "nodeID", Text: "123", Key: "0"},
+				},
+				EdgePoints: []data.Point{
+					{Type: "role", Text: "user"},
+				},
+			},
+				Children: nil,
+			},
+		},
+	}
 
-	if testReplaceNodes.ID == "123" {
+	client.ReplaceIDs(&testNodes, "parent122")
+
+	if testNodes.ID == "123" {
 		t.Fatal("ID not replaced")
 	}
 
-	if testReplaceNodes.Children[0].ID == "abc" {
+	if testNodes.Children[0].ID == "abc" {
 		t.Fatal("child ID not replaced")
 	}
 
 	// make sure nodes occur in multiple places, they have the same IDs
-	if testReplaceNodes.ID != testReplaceNodes.Children[0].Children[0].ID {
+	if testNodes.ID != testNodes.Children[0].Children[0].ID {
 		t.Fatal("123 IDs did not get replaced with the same value")
 	}
 
 	// mode sure any points of type nodeID get updated
-	if testReplaceNodes.Children[0].Points[1].Text == "123" {
+	if testNodes.Children[0].Points[1].Text == "123" {
 		t.Fatal("Points of type nodeID are not getting updated")
 	}
 
 	// make sure blank ids are handled correctly
-	if testReplaceNodes.Children[0].ID == testReplaceNodes.Children[1].ID {
+	if testNodes.Children[0].ID == testNodes.Children[1].ID {
 		t.Fatal("Blank node IDs not handled correctly")
 	}
 
 	// mode sure blank nodeID points are ignored
-	if testReplaceNodes.Points[0].Text != "" {
+	if testNodes.Points[0].Text != "" {
 		t.Fatal("Blank nodeID point not ignored")
 	}
 
-	if testReplaceNodes.Parent != "parent123" {
+	if testNodes.Parent != "parent123" {
 		t.Fatal("top level parent not correct")
 	}
 
-	if testReplaceNodes.ID != testReplaceNodes.Children[0].Parent {
+	if testNodes.ID != testNodes.Children[0].Parent {
 		t.Fatal("child parent not correct")
 	}
-
-	fmt.Printf("CLIFF: testReplaceNodes: %+v\n", testReplaceNodes)
 }
