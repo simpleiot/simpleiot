cb599d51d11bf802690e9822afde2644c5959dcb
Cliff Brake <cbrake@bec-systems.com>
2023-10-17 11:59:52 -0400

    fill in network manager conn details

--- frontend/src/Components/NodeNetworkManagerConn.elm
+++ frontend/src/Components/NodeNetworkManagerConn.elm
@@ -4,6 +4,7 @@ import Api.Point as Point
 import Components.NodeOptions exposing (NodeOptions, oToInputO)
 import Element exposing (..)
 import Element.Border as Border
+import Element.Font as Font
 import UI.Icon as Icon
 import UI.NodeInputs as NodeInputs
 import UI.Style as Style
@@ -33,8 +34,41 @@ view o =
 
                         textInput =
                             NodeInputs.nodeTextInput opts "0"
+
+                        textInputKey =
+                            NodeInputs.nodeTextInput opts
+
+                        checkboxInput =
+                            NodeInputs.nodeCheckboxInput opts "0"
+
+                        checkboxInputKey =
+                            NodeInputs.nodeCheckboxInput opts
+
+                        numberInput =
+                            NodeInputs.nodeNumberInput opts "0"
+
+                        numberInputKey =
+                            NodeInputs.nodeNumberInput opts
                     in
                     [ textInput Point.typeDescription "Description" ""
+                    , checkboxInput "disabled" "Disabled"
+                    , textInput "interface" "Interface" ""
+                    , checkboxInput "autoConnect" "Auto connect"
+                    , numberInput "autoConnectPriority" "Auto conn priority"
+                    , el [ Font.bold, centerX ] <| text "IPv4 Settings"
+                    , checkboxInputKey "ipv4Config" "staticIP" "Static IP"
+                    , textInputKey "ipv4Config" "address" "Address" "1.2.3.4"
+                    , textInputKey "ipv4Config" "netmask" "Netmask" "255.255.0.0"
+                    , textInputKey "ipv4Config" "gateway" "Gateway" "10.0.0.1"
+                    , textInputKey "ipv4Config" "dnsServer1" "DNS Server 1" "8.8.8.8"
+                    , textInputKey "ipv4Config" "dnsServer2" "DNS Server 2" "8.8.4.4"
+                    , el [ Font.bold, centerX ] <| text "IPv6 Settings"
+                    , checkboxInputKey "ipv6Config" "staticIP" "Static IP"
+                    , textInputKey "ipv6Config" "address" "Address" ""
+                    , numberInputKey "ipv6Config" "prefix" "Prefix"
+                    , textInputKey "ipv6Config" "gateway" "Gateway" ""
+                    , textInputKey "ipv6Config" "dnsServer1" "DNS Server 1" ""
+                    , textInputKey "ipv6Config" "dnsServer2" "DNS Server 2" ""
                     ]
 
                 else
