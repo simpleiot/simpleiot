ded4234ccf3775de497d1b1ccd63fabbb0b3dcad
Cliff Brake <cbrake@bec-systems.com>
2024-04-01 17:27:54 -0400

    test code

---
+++ cmd/test-resp-reader-close/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/simpleiot/simpleiot/respreader"
+	"go.bug.st/serial"
+)
+
+func main() {
+	mode := &serial.Mode{
+		BaudRate: 115200,
+	}
+
+	for {
+		time.Sleep(time.Second * 3)
+		log.Println("================================")
+		port, err := serial.Open(os.Args[1], mode)
+		if err != nil {
+			log.Println("Error opening port")
+			continue
+		}
+
+		log.Println("Port opened")
+
+		r := respreader.NewReadWriteCloser(port, time.Second, time.Millisecond*10)
+
+		time.Sleep(time.Second * 3)
+
+		log.Println("Closing port")
+
+		r.Close()
+	}
+}
